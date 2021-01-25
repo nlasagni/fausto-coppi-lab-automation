2b1242f9ccb2750ceb4bc5c8b22dccd7697f5f0e
alessia <alessia.cerami@studio.unibo.it>
2021-01-25 18:55:33 +0100

    added main class in build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     //In order to build a Kotlin project with Gradle:
     kotlin("jvm") version "1.4.21"
+    application
 }
 
 repositories {
@@ -11,4 +12,9 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    // Define the main class for the application
+    mainClass.set("faustoCoppiLabAutomation.MainClassKt")
 }
