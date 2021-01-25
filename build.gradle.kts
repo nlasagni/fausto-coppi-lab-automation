e1d69b85fcd21fd50e93f42649c212fdcc2296ec
alessia <alessia.cerami@studio.unibo.it>
2021-01-25 20:45:14 +0100

    Added semantic versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
     //In order to build a Kotlin project with Gradle:
     kotlin("jvm") version "1.4.21"
+    // A Gradle plugin that forces semantic versioning and relies on git to detect the project state
+    id("org.danilopianini.git-sensitive-semantic-versioning") version "0.2.3"
     application
+}
+
+gitSemVer {
+    version = computeGitSemVer()
 }
 
 repositories {
