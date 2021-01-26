b7720cde670ce3b0e87104ad356c09bd023f6e28
alessia <alessia.cerami@studio.unibo.it>
2021-01-26 21:37:34 +0100

    Test class with kotest

--- src/test/kotlin/faustoCoppiLabAutomation/MainTest.kt
+++ src/test/kotlin/faustoCoppiLabAutomation/MainTest.kt
@@ -1,10 +1,29 @@
 package faustoCoppiLabAutomation
 
-import kotlin.test.Test
-import kotlin.test.assertTrue
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import org.gradle.internal.impldep.org.junit.rules.TemporaryFolder
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import java.io.File
+import java.io.InputStream
 
-class MainTest {
-    @Test fun emptyTest() {
-        assertTrue(true, "This message should not be seen!")
+class MainTest : FreeSpec({
+    fun projectSetup(content: String) = TemporaryFolder().apply {
+        create()
+        newFile("build.gradle.kts").writeText(content)
     }
-}
+
+    fun projectSetup(content: () -> String) = projectSetup(content())
+
+    fun InputStream.toFile(file: File) {
+        use { input ->
+            file.outputStream().use { input.copyTo(it) }
+        }
+    }
+
+    "Nothing to print" - {
+        assert(true)
+    }
+})
