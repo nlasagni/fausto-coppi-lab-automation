10469995f3bbff9319403f9ceb09d710c8fa70cd
alessia <alessia.cerami@studio.unibo.it>
2021-02-02 21:26:01 +0100

    extended Reservation abstractClass

--- src/main/kotlin/it/unibo/lss/fcla/reservation/ReservationConsulting.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/ReservationConsulting.kt
@@ -1,8 +1,17 @@
 package it.unibo.lss.fcla.reservation
 
-data class ReservationConsulting(
+import java.util.Date
+
+class ReservationConsulting(
+    override val date: Date,
+    override val member: Member,
     val professional: Professional,
     val name: Consulting
-) {
+): Reservation(date, member) {
+
+    override fun updateDate(newDate: Date): Reservation {
+        return ReservationConsulting(newDate, member, professional, name)
+    }
+
     override fun toString(): String = "Reservation consulting $name with $professional"
 }
