79e87a9cf3c453c52b011336711d33a5864641c1
alessia <alessia.cerami@studio.unibo.it>
2021-02-02 21:28:09 +0100

    added basic desk class

---
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/Desk.kt
@@ -0,0 +1,26 @@
+package it.unibo.lss.fcla.reservation
+
+import java.util.*
+
+class Desk {
+
+    fun createGymRes(date: Date, member: Member, machine: Machine?) {
+        TODO()
+    }
+
+    fun createConsRes(date:Date, member: Member, professional: Professional, consulting: Consulting) {
+        TODO()
+    }
+
+    fun read(member: Member?, date: Date?) {
+        TODO()
+    }
+
+    fun update(reservation: Reservation, date: Date) {
+        TODO()
+    }
+
+    fun delete(reservation: Reservation) {
+        TODO()
+    }
+}
