7e7fe71504ff040614c928d247775e1b52885fa6
alessia <alessia.cerami@studio.unibo.it>
2021-02-02 19:56:32 +0100

    added professional data class

---
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/Professional.kt
@@ -0,0 +1,9 @@
+package it.unibo.lss.fcla.reservation
+
+data class Professional(
+    val type: ProfessionalType,
+    val firstName: String,
+    val lastName: String
+) {
+    override fun toString(): String = "Professional figure: [$type, $firstName, $lastName]"
+}
