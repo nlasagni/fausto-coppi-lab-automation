8f128c0788735b05914a382afeedfe3a9ffc0fe4
Andrea <andrea.giordano5@studio.unibo.it>
2021-02-02 19:33:15 +0100

    added membership card val into Member

--- src/main/kotlin/it/unibo/lss/fcla/reservation/Member.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/Member.kt
@@ -2,7 +2,8 @@ package it.unibo.lss.fcla.reservation
 
 data class Member(
         val firstName: String,
-        val lastName: String
+        val lastName: String,
+        val membershipCard: MembershipCard
 ) {
     override fun toString(): String = "This member is $firstName $lastName"
 }
