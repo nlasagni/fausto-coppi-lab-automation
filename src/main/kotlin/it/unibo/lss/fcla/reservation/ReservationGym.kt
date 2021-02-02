ab39bbc13c1eb34a4505ecadbaa7b2216e78cec1
Andrea <andrea.giordano5@studio.unibo.it>
2021-02-02 22:52:31 +0100

    fixed nullable date in ReservationGym

--- src/main/kotlin/it/unibo/lss/fcla/reservation/ReservationGym.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/ReservationGym.kt
@@ -5,7 +5,7 @@ import java.util.Date
 class ReservationGym(
         override val date: Date,
         override val member: Member,
-        val machine: Machine) : Reservation(date, member) {
+        val machine: Machine?) : Reservation(date, member) {
 
     override fun updateDate(newDate: Date): Reservation {
         return ReservationGym(newDate,member,machine)
