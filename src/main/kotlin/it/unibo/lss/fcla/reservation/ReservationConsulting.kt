032dc40acd52919a0782dcb9be347afba98337f9
Andrea <andrea.giordano5@studio.unibo.it>
2021-02-03 01:10:44 +0100

    overrided equals and hashcode in ReservationConsulting

--- src/main/kotlin/it/unibo/lss/fcla/reservation/ReservationConsulting.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/ReservationConsulting.kt
@@ -14,4 +14,16 @@ class ReservationConsulting(
     }
 
     override fun toString(): String = "Reservation consulting $name with $professional"
+
+    override fun equals(other: Any?): Boolean {
+        return if (other == null && other !is ReservationConsulting) false else other.hashCode() == hashCode()
+    }
+
+    override fun hashCode(): Int {
+        var result = date.hashCode()
+        result = 31 * result + member.hashCode()
+        result = 31 * result + professional.hashCode()
+        result = 31 * result + name.hashCode()
+        return result
+    }
 }
