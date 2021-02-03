b0fd4d9b104d8de8d9006ea33e61ecfccdb37dae
alessia <alessia.cerami@studio.unibo.it>
2021-02-03 21:00:02 +0100

    professional javadoc

--- src/main/kotlin/it/unibo/lss/fcla/reservation/Professional.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/Professional.kt
@@ -1,5 +1,12 @@
 package it.unibo.lss.fcla.reservation
 
+/**
+ * Class that represents the professional figure for a consulting
+ *
+ * @param type The type of fìprofessional figure
+ * @param firstName First name of a professional
+ * @param lastName Surname of a professional
+ */
 data class Professional(val type: ProfessionalType, val firstName: String, val lastName: String) {
     override fun toString(): String = "Professional figure: [$type, $firstName, $lastName]"
 }
--- src/main/kotlin/it/unibo/lss/fcla/reservation/ProfessionalType.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/ProfessionalType.kt
@@ -1,5 +1,8 @@
 package it.unibo.lss.fcla.reservation
 
+/**
+ * The professional figure that can be used
+ */
 enum class ProfessionalType {
     NUTRITIONIST,
     BIOMECHANICAL,
