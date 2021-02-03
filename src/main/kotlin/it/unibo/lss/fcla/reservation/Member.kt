6dbfde8496d9b8383d765ab5fb6881df8381159f
alessia <alessia.cerami@studio.unibo.it>
2021-02-03 20:59:46 +0100

    member javadoc

--- src/main/kotlin/it/unibo/lss/fcla/reservation/Member.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/Member.kt
@@ -1,5 +1,12 @@
 package it.unibo.lss.fcla.reservation
 
+/**
+ * Class used to identify a member
+ *
+ * @param firstName The name of the member
+ * @param lastName The surname of the member
+ * @param membershipCard The code of member's card
+ */
 data class Member(val firstName: String, val lastName: String, val membershipCard: MembershipCard) {
     override fun toString(): String = "Member $firstName $lastName"
 }
