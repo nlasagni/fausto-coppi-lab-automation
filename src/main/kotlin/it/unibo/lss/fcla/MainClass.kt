37287bc27e843d2920b5324eadef2ce80983842d
alessia <alessia.cerami@studio.unibo.it>
2021-02-04 16:44:33 +0100

    basic main prototype

--- src/main/kotlin/it/unibo/lss/fcla/MainClass.kt
+++ src/main/kotlin/it/unibo/lss/fcla/MainClass.kt
@@ -1,12 +1,36 @@
 package it.unibo.lss.fcla
 
+import it.unibo.lss.fcla.reservation.Desk
+import it.unibo.lss.fcla.reservation.Machine
+import it.unibo.lss.fcla.reservation.Member
+import it.unibo.lss.fcla.reservation.MembershipCard
+import java.util.Calendar
+
 class MainClass {
-    val hello: String
-        get() {
-            return "Hello world!!"
-        }
+
+    companion object MainClass {
+        const val YEAR = 2021
+        const val MONTH = 2
+        const val DAY = 25
+    }
+
+    fun basicReservation() {
+        val cal = Calendar.getInstance()
+        val cardCode = "2025"
+        val member = Member(
+            "Mario",
+            "Rossi",
+            MembershipCard(cardCode)
+        )
+        val machine = Machine("Leg press")
+        val desk = Desk()
+
+        cal.set(YEAR, MONTH, DAY)
+        desk.createGymReservation(cal.time, member, machine)
+        println("Reservation created: ${desk.read()}")
+    }
 }
 
 fun main() {
-    println(MainClass().hello)
+    MainClass().basicReservation()
 }
