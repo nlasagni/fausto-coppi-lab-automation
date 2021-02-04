3129b3eb6160740cd5014a92caad5942e2f4e019
alessia <alessia.cerami@studio.unibo.it>
2021-02-04 17:08:16 +0100

    added method to basic main prototype

--- src/main/kotlin/it/unibo/lss/fcla/MainClass.kt
+++ src/main/kotlin/it/unibo/lss/fcla/MainClass.kt
@@ -1,36 +1,51 @@
 package it.unibo.lss.fcla
 
+import it.unibo.lss.fcla.reservation.Consulting
 import it.unibo.lss.fcla.reservation.Desk
 import it.unibo.lss.fcla.reservation.Machine
 import it.unibo.lss.fcla.reservation.Member
 import it.unibo.lss.fcla.reservation.MembershipCard
+import it.unibo.lss.fcla.reservation.Professional
+import it.unibo.lss.fcla.reservation.ProfessionalType
 import java.util.Calendar
 
 class MainClass {
 
     companion object MainClass {
         const val YEAR = 2021
-        const val MONTH = 2
         const val DAY = 25
-    }
-
-    fun basicReservation() {
-        val cal = Calendar.getInstance()
-        val cardCode = "2025"
+        const val HOUR = 11
+        const val MINUTE = 15
+        val cal: Calendar = Calendar.getInstance()
+        private const val cardCode = "2025"
         val member = Member(
             "Mario",
             "Rossi",
             MembershipCard(cardCode)
         )
         val machine = Machine("Leg press")
+        val professional = Professional(
+            ProfessionalType.ATHLETIC_TRAINER,
+            "Mimmo",
+            "Bianchi"
+        )
         val desk = Desk()
+    }
 
-        cal.set(YEAR, MONTH, DAY)
+    fun basicGymReservation() {
+        cal.set(YEAR, Calendar.FEBRUARY, DAY, HOUR, MINUTE)
         desk.createGymReservation(cal.time, member, machine)
+        println("Reservation created: ${desk.read()}")
+    }
+
+    fun basicConsultingReservation() {
+        cal.set(YEAR, Calendar.MARCH, DAY, HOUR, MINUTE)
+        desk.createConsultingReservation(cal.time, member, professional, Consulting.ATHLETIC_TRAINING)
         println("Reservation created: ${desk.read()}")
     }
 }
 
 fun main() {
-    MainClass().basicReservation()
+    MainClass().basicGymReservation()
+    MainClass().basicConsultingReservation()
 }
