75c96af4b5529770fb5d70f3afeb4634cdd356d7
alessia <alessia.cerami@studio.unibo.it>
2021-02-08 18:29:49 +0100

    added task to generate jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+val myMainClass = "it.unibo.lss.fcla.MainClassKt"
+
 plugins {
     // In order to build a Kotlin project with Gradle:
     kotlin("jvm")
@@ -19,15 +21,10 @@ repositories {
     jcenter()
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        allWarningsAsErrors = true
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
-    }
-}
-
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.50")
 
     testImplementation(gradleTestKit())
     testImplementation("io.kotest:kotest-runner-junit5:4.2.5")
@@ -42,6 +39,33 @@ detekt {
     buildUponDefaultConfig = true
 }
 
+tasks.jar {
+    // Otherwise it throws a "No main manifest attribute" error
+    manifest {
+        attributes(
+            mapOf(
+                "Main-Class" to myMainClass,
+                "Implementation-Version" to archiveVersion
+            )
+        )
+    }
+    // All the dependencies needed by our application doesn't exists in the same classpath where our .jar resides.
+    // In order to make our application standalone, we must include all of its dependencies inside our application.
+    // Include all of its dependencies inside our application
+
+    // To add all of the dependencies otherwise a "NoClassDefFoundError" error
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+
+    // Configurations.compile is a reference to the all of the artifacts defined in the compile configuration.
+    from({
+        // Wrap each of the JAR files with the zipTree() method. The result is a collection of ZIP file trees
+
+        // configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+        configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
+    })
+}
+
 tasks.withType<Test> {
     useJUnitPlatform() // Use JUnit 5 engine
     testLogging.showStandardStreams = true
@@ -51,6 +75,13 @@ tasks.withType<Test> {
         showStandardStreams = true
         events(*org.gradle.api.tasks.testing.logging.TestLogEvent.values())
         exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        allWarningsAsErrors = true
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 }
 
@@ -74,5 +105,5 @@ tasks.register("computeVersion") {
 
 application {
     // Define the main class for the application
-    mainClass.set("it.unibo.lss.fcla.MainClassKt")
+    mainClass.set(myMainClass)
 }
