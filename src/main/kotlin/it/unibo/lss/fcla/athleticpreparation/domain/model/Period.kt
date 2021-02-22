3fab52e208a967c62ac5a5fa80afed655c7610ed
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-02-22 18:32:59 +0100

    Renames value objects by removing entity name as  prefix

--- src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/domain/model/Period.kt
+++ src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/domain/model/Period.kt
@@ -3,5 +3,5 @@ package it.unibo.lss.fcla.athleticpreparation.domain.model
 /**
  * @author Nicola Lasagni on 22/02/2021.
  */
-class TrainingPlanFrequency {
+class Period {
 }
--- src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/domain/model/TrainingPlanPurpose.kt
+++
@@ -1,7 +0,0 @@
-package it.unibo.lss.fcla.athleticpreparation.domain.model
-
-/**
- * @author Nicola Lasagni on 22/02/2021.
- */
-class TrainingPlanPurpose {
-}
