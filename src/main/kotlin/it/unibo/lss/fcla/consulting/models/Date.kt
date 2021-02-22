7d0062133d2c1f0995418a1054d45114780237b6
Stefano <stefano.braggion@me.com>
2021-02-22 23:17:15 +0100

    date value object

---
+++ src/main/kotlin/it/unibo/lss/fcla/consulting/models/Date.kt
@@ -0,0 +1,8 @@
+package it.unibo.lss.fcla.consulting.models
+
+/**
+ * Value object representing a date in consulting context
+ */
+data class Date(val year: Int, val month: Int, val day: Int){
+    
+}
