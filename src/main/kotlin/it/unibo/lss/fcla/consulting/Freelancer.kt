3a03317fda2e63be9808e006bcddd463b91d33a3
Stefano <stefano.braggion@me.com>
2021-02-22 22:46:14 +0100

    refactoring freelancer role

--- src/main/kotlin/it/unibo/lss/fcla/consulting/Freelancer.kt
+++ src/main/kotlin/it/unibo/lss/fcla/consulting/Freelancer.kt
@@ -1,16 +1,6 @@
 package it.unibo.lss.fcla.consulting
 
 /**
- * Freelancer role as enumeration
- */
-sealed class FreelancerRole{
-    data class Physiotherapist(val name: String = "Physiotherapist"): FreelancerRole()
-    data class AthleticTrainer(val name: String = "AthleticTrainer"): FreelancerRole()
-    data class Nutritionist(val name: String = "Nutritionist"): FreelancerRole()
-    data class Biomechanical(val name: String = "Biomechanical"): FreelancerRole()
-}
-
-/**
  * Represents a freelancer
  */
 class Freelancer(val firstName: String, val lastName: String) {
