a3e56ce558a4299fdcfcae9d76cb30fa5b371f63
Stefano <stefano.braggion@me.com>
2021-02-23 23:57:41 +0100

    interface abstraction for message and events

---
+++ src/main/kotlin/it/unibo/lss/fcla/consulting/interfaces/DomainEvent.kt
@@ -0,0 +1,8 @@
+package it.unibo.lss.fcla.consulting.interfaces
+
+/**
+ * @author Stefano Braggion
+ *
+ *
+ */
+interface DomainEvent : DomainMessage
---
+++ src/main/kotlin/it/unibo/lss/fcla/consulting/interfaces/DomainMessage.kt
@@ -0,0 +1,8 @@
+package it.unibo.lss.fcla.consulting.interfaces
+
+/**
+ * @author Stefano Braggion
+ *
+ * Top level interface that define the abstraction for a message (event or command)
+ */
+interface DomainMessage
