a5dfee164d4cdfb5ceea14a78b2304bb15111c5a
Stefano <stefano.braggion@me.com>
2021-02-23 23:51:57 +0100

    consulting summary created event

--- src/main/kotlin/it/unibo/lss/fcla/consulting/domainevents/ConsultingSummaryCreatedEvent.kt
+++ src/main/kotlin/it/unibo/lss/fcla/consulting/domainevents/ConsultingSummaryCreatedEvent.kt
@@ -1,7 +1,15 @@
 package it.unibo.lss.fcla.consulting.domainevents
 
 import it.unibo.lss.fcla.consulting.models.ConsultingId
+import it.unibo.lss.fcla.consulting.models.Date
 
+/**
+ * @author Stefano Braggion
+ *
+ * Event representing a created consulting summary
+ */
 data class ConsultingSummaryCreatedEvent(
-    val consultingId: ConsultingId
+    val consultingId: ConsultingId,
+    val consultingType: String,
+    val consultingDate: Date
 )
