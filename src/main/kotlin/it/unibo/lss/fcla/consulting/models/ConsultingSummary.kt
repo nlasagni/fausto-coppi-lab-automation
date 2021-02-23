5165af5c4a6f3c9b82ae802ceb0e59f89ef35b27
Stefano <stefano.braggion@me.com>
2021-02-23 23:06:40 +0100

    refactoring consulting summary validation

--- src/main/kotlin/it/unibo/lss/fcla/consulting/models/ConsultingSummary.kt
+++ src/main/kotlin/it/unibo/lss/fcla/consulting/models/ConsultingSummary.kt
@@ -10,8 +10,6 @@ import it.unibo.lss.fcla.consulting.exceptions.ConsultingException
 class ConsultingSummary(val consultingType: String, val freelancer: Freelancer, val description: String) {
 
     init {
-        if (description.isNullOrEmpty()) {
-            throw ConsultingException("A consulting summary with no description is not valid")
-        }
+        require(!description.isNullOrEmpty())
     }
 }
