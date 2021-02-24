d12aca66d9569d76288c52ca5e1491608c0a4136
alessia <alessia.cerami@studio.unibo.it>
2021-02-24 23:13:51 +0100

    added common consulting interface

--- src/main/kotlin/it/unibo/lss/fcla/reservation/common/ConsultingReservation.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/common/ConsultingReservation.kt
@@ -1,5 +1,13 @@
 package it.unibo.lss.fcla.reservation.common
 
+import java.util.Date
+
+/**
+ * This is the interface of a Consulting Reservation
+ * @param date The date of the requested consulting
+ * @param freelancerId The freelancer who will make the consulting
+ */
 interface ConsultingReservation {
-    
+    val date:Date
+    val freelancerId: String
 }
