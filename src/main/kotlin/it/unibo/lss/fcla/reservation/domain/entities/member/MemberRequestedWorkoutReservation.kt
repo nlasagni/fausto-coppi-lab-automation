8fc032ef724a297ae5a97cf9722c230f6228aa48
alessia <alessia.cerami@studio.unibo.it>
2021-02-25 18:46:56 +0100

    added member requested workout class

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/member/MemberRequestedWorkoutReservation.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/member/MemberRequestedWorkoutReservation.kt
@@ -1,3 +1,28 @@
 package it.unibo.lss.fcla.reservation.domain.entities.member
 
-class MemberRequestedWorkoutReservation
+import it.unibo.lss.fcla.reservation.common.WorkoutReservation
+
+class MemberRequestedWorkoutReservation {
+    private var workoutReservationList: MutableList<WorkoutReservation> = mutableListOf()
+
+    /**
+     * This method is used to add a [workoutReservation] into the list of all member reservations
+     */
+    fun addWorkoutReservation(workoutReservation: WorkoutReservation) {
+        this.workoutReservationList.add(workoutReservation)
+    }
+
+    /**
+     * This method is used to remove a [workoutReservation] from the list of all member reservations
+     */
+    fun deleteWorkoutReservation(workoutReservation: WorkoutReservation) {
+        this.workoutReservationList.remove(workoutReservation)
+    }
+
+    /**
+     * This method is used to return the list of [WorkoutReservation] of this member
+     */
+    fun getAllMemberWorkout(): List<WorkoutReservation> {
+        return workoutReservationList.toList()
+    }
+}
