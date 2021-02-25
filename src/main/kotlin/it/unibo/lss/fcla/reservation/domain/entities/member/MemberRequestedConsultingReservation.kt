0f64a6869790a05851d407d0a6eee57660f8fea1
alessia <alessia.cerami@studio.unibo.it>
2021-02-25 18:47:10 +0100

    added member requested consulting reservation class

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/member/MemberRequestedConsultingReservation.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/member/MemberRequestedConsultingReservation.kt
@@ -1,3 +1,28 @@
 package it.unibo.lss.fcla.reservation.domain.entities.member
 
-class MemberRequestedConsultingReservation
+import it.unibo.lss.fcla.reservation.common.ConsultingReservation
+
+class MemberRequestedConsultingReservation {
+    private var consultingReservationList: MutableList<ConsultingReservation> = mutableListOf()
+
+    /**
+     * This method is used to add a [consultingReservation] into the list of all member reservations
+     */
+    fun addConsultingReservation(consultingReservation: ConsultingReservation) {
+        this.consultingReservationList.add(consultingReservation)
+    }
+
+    /**
+     * This method is used to remove a [consultingReservation] from the list of all member reservations
+     */
+    fun deleteWorkoutReservation(consultingReservation: ConsultingReservation) {
+        this.consultingReservationList.remove(consultingReservation)
+    }
+
+    /**
+     * This method is used to return the list of [ConsultingReservation] of this member
+     */
+    fun getAllMemberConsulting(): List<ConsultingReservation> {
+        return consultingReservationList.toList()
+    }
+}
