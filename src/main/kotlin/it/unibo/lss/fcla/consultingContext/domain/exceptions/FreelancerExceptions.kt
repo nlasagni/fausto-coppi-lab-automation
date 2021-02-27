f0b9a121bfe3544f86149ac586b661da8537c0e5
Stefano <stefano.braggion@me.com>
2021-02-27 17:37:42 +0100

    added freelancer specific exceptions

---
+++ src/main/kotlin/it/unibo/lss/fcla/consultingContext/domain/exceptions/FreelancerExceptions.kt
@@ -0,0 +1,11 @@
+package it.unibo.lss.fcla.consultingContext.domain.exceptions
+
+/**
+ *
+ */
+class FreelancerFirstNameCannotBeNull: ConsultingException("Freelancer fist name cannot be null or empty")
+
+/**
+ *
+ */
+class FreelancerLastNameCannotBeNull: ConsultingException("Freelancer last name cannot be null or empty")
