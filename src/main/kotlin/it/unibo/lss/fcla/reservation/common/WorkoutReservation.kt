1ff951e7cb02e46b6c64258e6a6227602ab88491
alessia <alessia.cerami@studio.unibo.it>
2021-02-28 19:17:05 +0100

    refactored workout reservation interface deleting useless methods

--- src/main/kotlin/it/unibo/lss/fcla/reservation/common/WorkoutReservation.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/common/WorkoutReservation.kt
@@ -1,13 +1,10 @@
 package it.unibo.lss.fcla.reservation.common
 
 import java.util.Date
+import java.util.UUID
 
 interface WorkoutReservation {
     val aim: String
     val date: Date
-    val id: String
-
-    fun updateWorkoutReservationDate(date: Date): WorkoutReservation
-
-    fun updateWorkoutReservationAim(aim: String): WorkoutReservation
+    val id: UUID
 }
