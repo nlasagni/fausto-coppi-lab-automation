4840b2b446efbe9d513811b543257a8221650d09
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-02-28 17:36:53 +0100

    Makes all fields of Purpose value object private

--- src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/domain/model/Purpose.kt
+++ src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/domain/model/Purpose.kt
@@ -5,7 +5,7 @@ package it.unibo.lss.fcla.athleticpreparation.domain.model
  */
 sealed class Purpose {
     data class Strengthening(private val name: String = "Strengthening") : Purpose()
-    data class InjuryRecovery(val name: String = "Injury Recovery") : Purpose()
-    data class Discharge(val name: String = "Discharge") : Purpose()
-    data class Recovery(val name: String = "Recovery") : Purpose()
+    data class InjuryRecovery(private val name: String = "Injury Recovery") : Purpose()
+    data class Discharge(private val name: String = "Discharge") : Purpose()
+    data class Recovery(private val name: String = "Recovery") : Purpose()
 }
