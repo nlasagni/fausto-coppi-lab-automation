6da1a68865875b46d451f386d9f3b2fe99d8c271
Andrea <andrea.giordano5@studio.unibo.it>
2021-02-28 21:54:46 +0100

    Implemented Agenda

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/agenda/Agenda.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/agenda/Agenda.kt
@@ -1,3 +1,36 @@
 package it.unibo.lss.fcla.reservation.domain.entities.agenda
 
-class Agenda
+import it.unibo.lss.fcla.reservation.common.ConsultingReservation
+import it.unibo.lss.fcla.reservation.common.WorkoutReservation
+
+class Agenda(
+    private val agendaWorkoutReservation: AgendaWorkoutReservation,
+    private val agendaConsultingReservation: AgendaConsultingReservation
+) {
+
+    constructor() : this(AgendaWorkoutReservation(),AgendaConsultingReservation())
+
+    fun addConsultingReservation(reservation: ConsultingReservation): Agenda {
+        return Agenda(agendaWorkoutReservation, agendaConsultingReservation.addConsultingReservation(reservation))
+    }
+
+    fun addWorkoutReservation(reservation: WorkoutReservation): Agenda {
+        return Agenda(agendaWorkoutReservation.addWorkoutReservation(reservation), agendaConsultingReservation)
+    }
+
+    fun deleteConsultingReservation(reservation: ConsultingReservation): Agenda {
+        return Agenda(agendaWorkoutReservation, agendaConsultingReservation.deleteConsultingReservation(reservation))
+    }
+
+    fun deleteWorkoutReservation(reservation: WorkoutReservation): Agenda {
+        return Agenda(agendaWorkoutReservation.deleteWorkoutReservation(reservation), agendaConsultingReservation)
+    }
+
+    fun retrieveConsultingReservation(): List<ConsultingReservation> {
+        return agendaConsultingReservation.consultingReservationList
+    }
+
+    fun retrieveWorkoutReservation(): List<WorkoutReservation> {
+        return agendaWorkoutReservation.workoutReservationList
+    }
+}
