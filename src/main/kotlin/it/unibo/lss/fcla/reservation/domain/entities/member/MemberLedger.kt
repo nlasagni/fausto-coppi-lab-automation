f837a5172d674cb815e4ee1a93e6f2dc5bc9a9a4
alessia <alessia.cerami@studio.unibo.it>
2021-03-01 17:17:34 +0100

    added id to member ledger

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/member/MemberLedger.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/member/MemberLedger.kt
@@ -1,14 +1,16 @@
 package it.unibo.lss.fcla.reservation.domain.entities.member
 
-class MemberLedger(private val members: List<Member>) {
+import java.util.*
 
-    constructor() : this(listOf<Member>())
+class MemberLedger(private val members: List<Member>, val id: UUID) {
+
+    constructor(id: UUID) : this(listOf<Member>(), id)
 
     /**
      * Returns a [MemberLedger] adding a new [member] into the list of members
      */
     fun addMemberToLedger(member: Member): MemberLedger {
-        return MemberLedger(members + member)
+        return MemberLedger(members + member, id)
     }
 
     /**
--- src/test/kotlin/it/unibo/lss/fcla/reservation/domain/entities/member/MemberLedgerTest.kt
+++ src/test/kotlin/it/unibo/lss/fcla/reservation/domain/entities/member/MemberLedgerTest.kt
@@ -4,12 +4,14 @@ import io.kotest.core.spec.style.FreeSpec
 import java.util.UUID
 
 class MemberLedgerTest : FreeSpec({
-    val memberList = MemberLedger()
+    val ledgerId = UUID.randomUUID()
+    val memberList = MemberLedger(ledgerId)
     val member = Member("Mario", "Rossi", UUID.randomUUID())
 
     "A member should" - {
         "be added to the ledger" - {
             val newMemberList = memberList.addMemberToLedger(member)
+            assert(memberList.id == newMemberList.id)
             assert(newMemberList.retrieveAllMembers().contains(member))
         }
     }
