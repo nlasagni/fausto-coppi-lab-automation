31f4c87b39745ea1a8aad54aaae238c88d1e2940
alessia <alessia.cerami@studio.unibo.it>
2021-03-01 16:37:59 +0100

    Added ID to agenda

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/agenda/Agenda.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/agenda/Agenda.kt
@@ -2,40 +2,58 @@ package it.unibo.lss.fcla.reservation.domain.entities.agenda
 
 import it.unibo.lss.fcla.reservation.common.ConsultingReservation
 import it.unibo.lss.fcla.reservation.common.WorkoutReservation
+import java.util.*
 
 class Agenda(
     private val agendaWorkoutReservation: AgendaWorkoutReservation,
-    private val agendaConsultingReservation: AgendaConsultingReservation
+    private val agendaConsultingReservation: AgendaConsultingReservation,
+    val id: UUID
 ) {
 
-    constructor() : this(AgendaWorkoutReservation(), AgendaConsultingReservation())
+    constructor(id: UUID) : this(AgendaWorkoutReservation(), AgendaConsultingReservation(), id)
 
     /**
      * Returns a new [Agenda] adding a [reservation] into the list of [ConsultingReservation]
      */
     fun addConsultingReservation(reservation: ConsultingReservation): Agenda {
-        return Agenda(agendaWorkoutReservation, agendaConsultingReservation.addConsultingReservation(reservation))
+        return Agenda(
+            agendaWorkoutReservation,
+            agendaConsultingReservation.addConsultingReservation(reservation),
+            id
+        )
     }
 
     /**
      * Returns a new [Agenda] adding a [reservation] into the list of [WorkoutReservation]
      */
     fun addWorkoutReservation(reservation: WorkoutReservation): Agenda {
-        return Agenda(agendaWorkoutReservation.addWorkoutReservation(reservation), agendaConsultingReservation)
+        return Agenda(
+            agendaWorkoutReservation.addWorkoutReservation(reservation),
+            agendaConsultingReservation,
+            id
+        )
     }
 
     /**
      * Returns a new [Agenda] deleting a [reservation] from the list of [ConsultingReservation]
      */
     fun deleteConsultingReservation(reservation: ConsultingReservation): Agenda {
-        return Agenda(agendaWorkoutReservation, agendaConsultingReservation.deleteConsultingReservation(reservation))
+        return Agenda(
+            agendaWorkoutReservation,
+            agendaConsultingReservation.deleteConsultingReservation(reservation),
+            id
+        )
     }
 
     /**
      * Returns a new [Agenda] deleting a [reservation] from the list of [WorkoutReservation]
      */
     fun deleteWorkoutReservation(reservation: WorkoutReservation): Agenda {
-        return Agenda(agendaWorkoutReservation.deleteWorkoutReservation(reservation), agendaConsultingReservation)
+        return Agenda(
+            agendaWorkoutReservation.deleteWorkoutReservation(reservation),
+            agendaConsultingReservation,
+            id
+        )
     }
 
     /**
