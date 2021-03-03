87946a52cc9cdf89ce138136dbee4ee3414b6d89
Andrea <andrea.giordano5@studio.unibo.it>
2021-03-04 00:14:18 +0100

    Fixed missing const

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/events/results/RequestFailedMessages.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/events/results/RequestFailedMessages.kt
@@ -1,6 +1,6 @@
 package it.unibo.lss.fcla.reservation.domain.usecases.events.results
 
 object RequestFailedMessages {
-    val reservationNotFound = "The requested reservation was not found."
-    val emptyWorkoutAim = "Unable to create a workout reservation due to empty aim."
+    const val reservationNotFound = "The requested reservation was not found."
+    const val emptyWorkoutAim = "Unable to create a workout reservation due to empty aim."
 }
