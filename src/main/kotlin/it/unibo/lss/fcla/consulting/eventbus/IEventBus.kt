3986fb0254d63339f5799394ecd1d3a482005662
Stefano <stefano.braggion@me.com>
2021-03-03 22:11:05 +0100

    clean up unused packages

--- src/main/kotlin/it/unibo/lss/fcla/consulting/eventbus/IEventBus.kt
+++
@@ -1,8 +0,0 @@
-package it.unibo.lss.fcla.consulting.eventbus
-
-import it.unibo.lss.fcla.consulting.domain.contracts.DomainEvent
-
-interface IEventBus {
-    fun publish(event: DomainEvent)
-    fun subscribe(eventHandler: IEventHandler)
-}
