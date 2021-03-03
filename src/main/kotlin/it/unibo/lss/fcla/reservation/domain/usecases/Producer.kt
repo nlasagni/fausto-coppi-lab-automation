56bd148eef0b3eeb55388705cbf4e3f0a1535d96
Andrea <andrea.giordano5@studio.unibo.it>
2021-03-03 23:42:59 +0100

    Fixed producer

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/Producer.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/Producer.kt
@@ -1,7 +1,8 @@
 package it.unibo.lss.fcla.reservation.domain.usecases
 
 import it.unibo.lss.fcla.reservation.common.Event
+import java.util.UUID
 
 interface Producer {
-    fun produce(event: Event): List<Event>
+    fun produce(event: Event): Map<UUID, List<Event>>
 }
