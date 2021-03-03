2e50defb30935fd01f4cd626a7c74b4c6a7c92f9
Andrea <andrea.giordano5@studio.unibo.it>
2021-03-03 23:44:26 +0100

    Implemented agendaProjection

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/projections/AgendaProjection.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/projections/AgendaProjection.kt
@@ -2,14 +2,21 @@ package it.unibo.lss.fcla.reservation.domain.usecases.projections
 
 import it.unibo.lss.fcla.reservation.common.Event
 import it.unibo.lss.fcla.reservation.domain.entities.agenda.Agenda
+import it.unibo.lss.fcla.reservation.domain.entities.events.agenda.AgendaAddConsultingReservationEvent
+import it.unibo.lss.fcla.reservation.domain.entities.events.agenda.AgendaAddWorkoutReservationEvent
+import it.unibo.lss.fcla.reservation.domain.entities.events.agenda.AgendaDeleteConsultingReservationEvent
+import it.unibo.lss.fcla.reservation.domain.entities.events.agenda.AgendaDeleteWorkoutReservationEvent
 import java.util.UUID
 
 class AgendaProjection(val init: Agenda) {
 
-    constructor() : this(Agenda(UUID.randomUUID()))
+    constructor(agendaId: UUID) : this(Agenda(agendaId))
 
-    fun update(event: Event): Agenda {
-        // TODO switch(event)
-        return init
+    fun update(agenda: Agenda, event: Event): Agenda = when (event) {
+        is AgendaAddConsultingReservationEvent -> agenda.addConsultingReservation(event.reservation)
+        is AgendaAddWorkoutReservationEvent -> agenda.addWorkoutReservation(event.reservation)
+        is AgendaDeleteConsultingReservationEvent -> agenda.deleteConsultingReservation(event.reservation)
+        is AgendaDeleteWorkoutReservationEvent -> agenda.deleteWorkoutReservation(event.reservation)
+        else -> agenda
     }
 }
