4635259038b9358f55b976a222f7423fe90f5bb0
alessia <alessia.cerami@studio.unibo.it>
2021-03-03 23:07:52 +0100

    member ledger retrieves member

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/member/MemberLedger.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/member/MemberLedger.kt
@@ -1,5 +1,7 @@
 package it.unibo.lss.fcla.reservation.domain.entities.member
 
+import it.unibo.lss.fcla.reservation.common.ConsultingReservation
+import it.unibo.lss.fcla.reservation.common.WorkoutReservation
 import java.util.UUID
 
 class MemberLedger(private val members: List<Member>, val id: UUID) {
@@ -18,5 +20,19 @@ class MemberLedger(private val members: List<Member>, val id: UUID) {
      */
     fun retrieveAllMembers(): List<Member> {
         return members.toList()
+    }
+
+    /**
+     * Returns the [Member] of a [consultingReservation]
+     */
+    fun retrieveMemberWithConsultingReservation(consultingReservation: ConsultingReservation) : Member {
+        return members.first {member -> member.retrieveConsultingReservation().contains(consultingReservation) }
+    }
+
+    /**
+     * Returns the [Member] of a [workoutReservation]
+     */
+    fun retrieveMemberWithWorkoutReservation(workoutReservation: WorkoutReservation) : Member {
+        return members.first {member -> member.retrieveWorkoutReservation().contains(workoutReservation) }
     }
 }
