029a30a18cb33fcc3ba2b48accdd70b7d5746c9c
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-03-03 22:58:08 +0100

    Introduces new exception for TrainingPlan

---
+++ src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/domain/exception/TrainingPlanMustBePreparedDuringPeriodOfPreparation.kt
@@ -0,0 +1,7 @@
+package it.unibo.lss.fcla.athleticpreparation.domain.exception
+
+/**
+ * @author Nicola Lasagni on 03/03/2021.
+ */
+class TrainingPlanMustBePreparedDuringPeriodOfPreparation :
+        Exception("A TrainingPlan must be prepared for a period inside the AthleticPreparation one.")
