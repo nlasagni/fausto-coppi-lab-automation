a3ce5e41d91fbb2b0ad453c653d37f42d6fa9f21
Andrea <andrea.giordano5@studio.unibo.it>
2021-03-03 23:42:10 +0100

    Added message to request result event

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/events/results/RequestSucceededEvent.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/events/results/RequestSucceededEvent.kt
@@ -3,4 +3,8 @@ package it.unibo.lss.fcla.reservation.domain.usecases.events.results
 import it.unibo.lss.fcla.reservation.common.Event
 import java.util.UUID
 
-data class RequestSucceededEvent(override val id: UUID, val requestId: UUID) : Event
+data class RequestSucceededEvent(
+    override val id: UUID,
+    val requestId: UUID,
+    val message: String = "Everything fine"
+) : Event
