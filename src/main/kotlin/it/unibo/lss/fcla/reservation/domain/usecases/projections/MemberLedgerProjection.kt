c2e4a44d115d731eeb7c7285bd87eeee975b8d47
alessia <alessia.cerami@studio.unibo.it>
2021-03-03 12:54:13 +0100

    added member projection

---
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/projections/MemberLedgerProjection.kt
@@ -0,0 +1,17 @@
+package it.unibo.lss.fcla.reservation.domain.usecases.projections
+
+import it.unibo.lss.fcla.reservation.common.Event
+import it.unibo.lss.fcla.reservation.domain.entities.events.member.LedgerAddMemberEvent
+import it.unibo.lss.fcla.reservation.domain.entities.member.Member
+import it.unibo.lss.fcla.reservation.domain.entities.member.MemberLedger
+import java.util.UUID
+
+class MemberLedgerProjection(val init: MemberLedger) {
+
+    constructor(ledgerId: UUID) : this(MemberLedger(ledgerId))
+
+    fun update(ledger: MemberLedger, event: Event): MemberLedger = when (event) {
+        is LedgerAddMemberEvent -> ledger.addMemberToLedger(Member(event.firstName, event.lastName, event.memberId))
+        else -> ledger
+    }
+}
--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/projections/MemberProjection.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/projections/MemberProjection.kt
@@ -1,14 +1,28 @@
 package it.unibo.lss.fcla.reservation.domain.usecases.projections
 
 import it.unibo.lss.fcla.reservation.common.Event
+import it.unibo.lss.fcla.reservation.domain.entities.events.member.MemberAddConsultingReservationEvent
+import it.unibo.lss.fcla.reservation.domain.entities.events.member.MemberAddWorkoutReservationEvent
+import it.unibo.lss.fcla.reservation.domain.entities.events.member.MemberDeleteConsultingReservationEvent
+import it.unibo.lss.fcla.reservation.domain.entities.events.member.MemberDeleteWorkoutReservationEvent
 import it.unibo.lss.fcla.reservation.domain.entities.member.Member
 import java.util.UUID
 
 class MemberProjection(val init: Member) {
-    constructor() : this(Member(firstName = "", lastName = "", UUID.randomUUID()))
 
-    fun update(event: Event): Member {
-        // TODO switch(event)
-        return init
+    constructor(memberName: String, memberSurname: String, memberId: UUID) : this(
+        Member(
+            memberName,
+            memberSurname,
+            memberId
+        )
+    )
+
+    fun update(member: Member, event: Event): Member = when (event) {
+        is MemberAddConsultingReservationEvent -> member.addConsultingReservation(event.reservation)
+        is MemberAddWorkoutReservationEvent -> member.addWorkoutReservation(event.reservation)
+        is MemberDeleteConsultingReservationEvent -> member.deleteConsultingReservation(event.reservation)
+        is MemberDeleteWorkoutReservationEvent -> member.deleteWorkoutReservation(event.reservation)
+        else -> member
     }
 }
