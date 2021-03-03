dda5e4e5570f0181ddddd62511a5c852c58071ce
Stefano <stefano.braggion@me.com>
2021-03-03 12:31:23 +0100

    create use cases skeleton class

---
+++ src/main/kotlin/it/unibo/lss/fcla/consulting/usecases/ConsultingUseCases.kt
@@ -0,0 +1,69 @@
+package it.unibo.lss.fcla.consulting.usecases
+
+/**
+ * @author Stefano Braggion
+ *
+ *
+ */
+class ConsultingUseCases {
+
+    companion object {
+
+        /**
+         * FCLAC-1 Examine consulting summaries
+         */
+        fun examineConsultingSummaries() {
+
+        }
+
+        /**
+         * FCLAC-7 Receive Consulting
+         */
+        fun receiveConsulting() {
+
+        }
+
+        /**
+         * FCLAC-8 Manage Consulting Summaries (Update)
+         */
+        fun updateConsultingSummary() {
+
+        }
+
+        /**
+         * FCLAC-8 Manage Consulting Summaries (Delete)
+         */
+        fun deleteConsultingSummary() {
+
+        }
+
+        /**
+         * FCLAC-9 Retrieve Profile
+         */
+        fun retrieveProfile() {
+
+        }
+
+        /**
+         *
+         */
+        fun updateFreelancerAvailabilityForDay() {
+
+        }
+
+        /**
+         *
+         */
+        fun createFreelancerAvailabilityForDay() {
+
+        }
+
+        /**
+         *
+         */
+        fun deleteFreelancerAvailabilityForDay() {
+
+        }
+    }
+
+}
