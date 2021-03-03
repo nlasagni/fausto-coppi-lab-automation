bf1c293503e6dad435617cf74d2f1666666d1cbe
Andrea <andrea.giordano5@studio.unibo.it>
2021-03-03 23:46:18 +0100

    Fixed WResMan constructor and companion obj, Added closeWRes method

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/WorkoutReservationManager.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/WorkoutReservationManager.kt
@@ -1,52 +1,118 @@
 package it.unibo.lss.fcla.reservation.domain.usecases
 
 import it.unibo.lss.fcla.reservation.common.Event
+import it.unibo.lss.fcla.reservation.domain.entities.agenda.Agenda
+import it.unibo.lss.fcla.reservation.domain.entities.events.agenda.AgendaAddWorkoutReservationEvent
+import it.unibo.lss.fcla.reservation.domain.entities.events.agenda.AgendaDeleteWorkoutReservationEvent
+import it.unibo.lss.fcla.reservation.domain.entities.events.member.LedgerAddMemberEvent
+import it.unibo.lss.fcla.reservation.domain.entities.events.member.MemberAddWorkoutReservationEvent
+import it.unibo.lss.fcla.reservation.domain.entities.events.member.MemberDeleteWorkoutReservationEvent
+import it.unibo.lss.fcla.reservation.domain.entities.exceptions.WorkoutReservationAimCannotBeEmpty
+import it.unibo.lss.fcla.reservation.domain.entities.member.MemberLedger
+import it.unibo.lss.fcla.reservation.domain.entities.reservation.CloseWorkoutReservation
 import it.unibo.lss.fcla.reservation.domain.usecases.events.requests.CloseWorkoutReservationEvent
 import it.unibo.lss.fcla.reservation.domain.usecases.events.requests.CreateWorkoutReservationEvent
 import it.unibo.lss.fcla.reservation.domain.usecases.events.requests.DeleteWorkoutReservationEvent
 import it.unibo.lss.fcla.reservation.domain.usecases.events.requests.UpdateWorkoutReservationEvent
+import it.unibo.lss.fcla.reservation.domain.usecases.events.results.RequestFailedEvent
+import it.unibo.lss.fcla.reservation.domain.usecases.events.results.RequestFailedMessages
+import java.util.UUID
 
-class WorkoutReservationManager() : Producer {
+class WorkoutReservationManager(private var agenda: Agenda, private var ledger: MemberLedger) : Producer {
 
-    private fun closeWorkoutReservation(event: CloseWorkoutReservationEvent): List<Event> {
-        /* TODO
-        - retrieve res
-        - create closedRes
-        - remove oldRes from agenda and member
-        - add newRes to agenda and member
-         */
-        return listOf()
+    constructor(agendaId: UUID, ledgerId: UUID, events: Map<UUID, List<Event>>) :
+        this(
+            computeAgenda(agendaId, events.getOrDefault(agendaId, listOf())),
+            computeMemberLedger(ledgerId, events.getOrDefault(ledgerId, listOf()))
+        )
+
+    // TODO Probably useless
+    constructor(agendaId: UUID, ledgerId: UUID) : this(agendaId, ledgerId, mapOf())
+
+    private companion object {
+        private fun computeAgenda(agendaId: UUID, events: List<Event>): Agenda {
+            // TODO compute Agenda using projection
+            return Agenda(agendaId)
+        }
+
+        private fun computeMemberLedger(ledgerId: UUID, events: List<Event>): MemberLedger {
+            // TODO compute MemberLedger using projection
+            return MemberLedger(ledgerId)
+        }
     }
 
-    private fun createWorkoutReservation(event: CreateWorkoutReservationEvent): List<Event> {
+    private fun closeWorkoutReservation(event: CloseWorkoutReservationEvent): Map<UUID, List<Event>> {
+        val retrievedReservation = agenda.retrieveWorkoutReservation()
+            .firstOrNull { workoutReservation -> event.reservationId == workoutReservation.id }
+        retrievedReservation ?: return mapOf(
+            event.id to listOf(
+                RequestFailedEvent(
+                    UUID.randomUUID(),
+                    event.id,
+                    RequestFailedMessages.reservationNotFound
+                )
+            )
+        )
+        val closedReservation: CloseWorkoutReservation
+        try {
+            closedReservation = CloseWorkoutReservation(
+                retrievedReservation.aim,
+                retrievedReservation.date,
+                retrievedReservation.id
+            )
+        } catch (exception: WorkoutReservationAimCannotBeEmpty) {
+            return mapOf(
+                event.id to listOf(
+                    RequestFailedEvent(
+                        UUID.randomUUID(),
+                        event.id,
+                        RequestFailedMessages.emptyWorkoutAim
+                    )
+                )
+            )
+        }
+        val agendaDeleteReservationEvent = AgendaDeleteWorkoutReservationEvent(UUID.randomUUID(), retrievedReservation)
+        val agendaAddReservationEvent = AgendaAddWorkoutReservationEvent(UUID.randomUUID(), closedReservation)
+        val member = ledger.retrieveMemberWithWorkoutReservation(retrievedReservation)
+        val memberDeleteReservationEvent = MemberDeleteWorkoutReservationEvent(UUID.randomUUID(), retrievedReservation)
+        val memberAddReservationEvent = MemberAddWorkoutReservationEvent(UUID.randomUUID(), closedReservation)
+        val ledgerAddMember = LedgerAddMemberEvent(UUID.randomUUID(), member.addWorkoutReservation(closedReservation))
+        return mapOf(
+            agenda.id to listOf(agendaDeleteReservationEvent, agendaAddReservationEvent),
+            member.id to listOf(memberDeleteReservationEvent, memberAddReservationEvent),
+            ledger.id to listOf(ledgerAddMember)
+        )
+    }
+
+    private fun createWorkoutReservation(event: CreateWorkoutReservationEvent): Map<UUID, List<Event>> {
         /* TODO
         - create res
         - add newRes to agenda and member
          */
-        return listOf()
+        return mapOf()
     }
 
-    private fun deleteWorkoutReservation(event: DeleteWorkoutReservationEvent): List<Event> {
+    private fun deleteWorkoutReservation(event: DeleteWorkoutReservationEvent): Map<UUID, List<Event>> {
         /* TODO
         - retrieve res
         - remove res from agenda and member
          */
-        return listOf()
+        return mapOf()
     }
 
-    private fun updateWorkoutReservation(event: UpdateWorkoutReservationEvent): List<Event> {
+    private fun updateWorkoutReservation(event: UpdateWorkoutReservationEvent): Map<UUID, List<Event>> {
         /* TODO
         - retrieve consulting
         - update cons
          */
-        return listOf()
+        return mapOf()
     }
 
-    override fun produce(event: Event): List<Event> = when (event) {
+    override fun produce(event: Event): Map<UUID, List<Event>> = when (event) {
         is CloseWorkoutReservationEvent -> closeWorkoutReservation(event)
         is CreateWorkoutReservationEvent -> createWorkoutReservation(event)
         is DeleteWorkoutReservationEvent -> deleteWorkoutReservation(event)
         is UpdateWorkoutReservationEvent -> updateWorkoutReservation(event)
-        else -> listOf()
+        else -> mapOf()
     }
 }
