72b19d4593e5ca216a6ae4a1ccf937e4f1f54a2c
alessia <alessia.cerami@studio.unibo.it>
2021-03-04 00:20:29 +0100

    improved memberLedger

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/member/MemberLedger.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/member/MemberLedger.kt
@@ -4,35 +4,37 @@ import it.unibo.lss.fcla.reservation.common.ConsultingReservation
 import it.unibo.lss.fcla.reservation.common.WorkoutReservation
 import java.util.UUID
 
-class MemberLedger(private val members: List<Member>, val id: UUID) {
+class MemberLedger(private val members: Map<UUID, Member>, val id: UUID) {
 
-    constructor(id: UUID) : this(listOf<Member>(), id)
+    constructor(id: UUID) : this(mapOf(), id)
 
     /**
      * Returns a [MemberLedger] adding a new [member] into the list of members
      */
     fun addMemberToLedger(member: Member): MemberLedger {
-        return MemberLedger(members + member, id)
+        return MemberLedger(members + (member.id to member), id)
     }
 
     /**
      * Returns a list of [Member]
      */
     fun retrieveAllMembers(): List<Member> {
-        return members.toList()
+        return members.values.toList()
     }
 
     /**
      * Returns the [Member] of a [consultingReservation]
      */
     fun retrieveMemberWithConsultingReservation(consultingReservation: ConsultingReservation): Member {
-        return members.first { member -> member.retrieveConsultingReservation().contains(consultingReservation) }
+        return members.values
+            .first { member -> member.retrieveConsultingReservation().contains(consultingReservation) }
     }
 
     /**
      * Returns the [Member] of a [workoutReservation]
      */
     fun retrieveMemberWithWorkoutReservation(workoutReservation: WorkoutReservation): Member {
-        return members.first { member -> member.retrieveWorkoutReservation().contains(workoutReservation) }
+        return members.values
+            .first { member -> member.retrieveWorkoutReservation().contains(workoutReservation) }
     }
 }
