f0e6c915a0772dfef0d46221014d454b1e421b02
Stefano <stefano.braggion@me.com>
2021-03-04 13:08:23 +0100

    green tests for event store of freelancer

--- src/test/kotlin/it/unibo/lss/fcla/consulting/EventTests.kt
+++ src/test/kotlin/it/unibo/lss/fcla/consulting/EventTests.kt
@@ -4,7 +4,10 @@ import io.kotest.core.spec.style.FreeSpec
 import it.unibo.lss.fcla.consulting.common.EventStore
 import it.unibo.lss.fcla.consulting.domain.consulting.ConsultingSummary
 import it.unibo.lss.fcla.consulting.domain.consulting.Date
+import it.unibo.lss.fcla.consulting.domain.freelancer.Freelancer
+import it.unibo.lss.fcla.consulting.domain.freelancer.FreelancerRole
 import it.unibo.lss.fcla.consulting.domain.member.Member
+import java.time.LocalTime
 
 class EventTests : FreeSpec ({
 
@@ -13,13 +16,38 @@ class EventTests : FreeSpec ({
         var aggregateRepository = MemberMockRepository(eventStore)
         val aggregateID = "123"
         val consultingID = "C1234"
+        val expectedEvents = 1
 
         val member = Member(aggregateID, firstName = "Alan", lastName = "Turing")
         member.receiveConsulting(consultingID, ConsultingSummary(consultingType = "TYPE",
-            description = "DESCRIPTION", consultingDate = Date(2021, 1, 1)))
+            description = "DESCRIPTION", consultingDate = Date(year = 2021,month = 1, day = 1)))
 
         aggregateRepository.save(member)
 
-        assert(eventStore.getEventsForAggregate(aggregateID).count() == 1)
+        assert(eventStore.getEventsForAggregate(aggregateID).count() == expectedEvents &&
+            member.getUncommittedEvents().count() == 0)
+    }
+
+    "Freelancer event store accumulation test" - {
+        var eventStore = EventStore()
+        var aggregateRepository = FreelancerMockRepository(eventStore)
+        val aggregateID = "123"
+        val date = Date(year = 2021, month = 1, day = 1)
+        val expectedEvents = 4
+
+        val freelancer = Freelancer.createFreelancer(aggregateID, firstName = "Alan",
+            lastName = "Turing", role = FreelancerRole.Biomechanical())
+
+        freelancer.addAvailability(newAvailabilityDate = date,
+        fromTime = LocalTime.MIN, toTime = LocalTime.MAX)
+
+        freelancer.updateAvailability(availabilityDate = date, fromTime = LocalTime.MIDNIGHT, toTime = LocalTime.MAX)
+
+        freelancer.deleteAvailability(availabilityDate = date)
+
+        aggregateRepository.save(freelancer)
+
+        assert(eventStore.getEventsForAggregate(aggregateID).count() == expectedEvents &&
+            freelancer.getUncommittedEvents().count() == 0)
     }
 })
--- src/test/kotlin/it/unibo/lss/fcla/consulting/MemberMockRepository.kt
+++
@@ -1,8 +0,0 @@
-package it.unibo.lss.fcla.consulting
-
-import it.unibo.lss.fcla.consulting.common.EventSourcedRepository
-import it.unibo.lss.fcla.consulting.common.EventStore
-import it.unibo.lss.fcla.consulting.domain.member.Member
-
-class MemberMockRepository(eventStore: EventStore) : EventSourcedRepository<Member>(eventStore) {
-}
