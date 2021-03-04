a709456516c2c08604d59f8b214da39fe9734519
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-03-04 18:47:49 +0100

    Removes Duration and adapts Exercise

--- src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/domain/model/Duration.kt
+++
@@ -1,6 +0,0 @@
-package it.unibo.lss.fcla.athleticpreparation.domain.model
-
-/**
- * @author Nicola Lasagni on 22/02/2021.
- */
-data class Duration(val value: Int)
--- src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/domain/model/Exercise.kt
+++ src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/domain/model/Exercise.kt
@@ -1,10 +1,12 @@
 package it.unibo.lss.fcla.athleticpreparation.domain.model
 
+import java.time.Duration
+
 /**
  * @author Nicola Lasagni on 28/02/2021.
  */
 data class Exercise(
         private val configuration: Configuration,
-        private val duration: Duration,
-        private val rest: Duration
+        private val durationOfExecution: Duration,
+        private val durationOfRest: Duration
 )
