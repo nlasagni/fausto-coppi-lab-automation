60249a5d5a85bb4e2888baa678f699eaf5adcdfb
Andrea <andrea.giordano5@studio.unibo.it>
2021-03-04 23:08:01 +0100

    Added partial UseCase implementation

---
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/ReservationUseCase.kt
@@ -0,0 +1,156 @@
+package it.unibo.lss.fcla.reservation.domain.usecases
+
+import it.unibo.lss.fcla.reservation.common.Event
+import it.unibo.lss.fcla.reservation.domain.entities.agenda.Agenda
+import it.unibo.lss.fcla.reservation.domain.entities.member.MemberLedger
+import it.unibo.lss.fcla.reservation.domain.entities.reservation.CloseConsultingReservation
+import it.unibo.lss.fcla.reservation.domain.entities.reservation.OpenConsultingReservation
+import it.unibo.lss.fcla.reservation.domain.usecases.events.requests.CloseConsultingReservationEvent
+import it.unibo.lss.fcla.reservation.domain.usecases.events.requests.CloseWorkoutReservationEvent
+import it.unibo.lss.fcla.reservation.domain.usecases.events.requests.CreateConsultingReservationEvent
+import it.unibo.lss.fcla.reservation.domain.usecases.events.requests.CreateWorkoutReservationEvent
+import it.unibo.lss.fcla.reservation.domain.usecases.events.requests.DeleteConsultingReservationEvent
+import it.unibo.lss.fcla.reservation.domain.usecases.events.requests.DeleteWorkoutReservationEvent
+import it.unibo.lss.fcla.reservation.domain.usecases.events.requests.UpdateConsultingReservationEvent
+import it.unibo.lss.fcla.reservation.domain.usecases.events.requests.UpdateWorkoutReservationEvent
+import it.unibo.lss.fcla.reservation.domain.usecases.events.results.RequestFailedEvent
+import it.unibo.lss.fcla.reservation.domain.usecases.events.results.RequestFailedMessages
+import it.unibo.lss.fcla.reservation.domain.usecases.events.results.RequestSucceededEvent
+import it.unibo.lss.fcla.reservation.domain.usecases.facades.ConsultingReservationFacade
+import it.unibo.lss.fcla.reservation.domain.usecases.projections.AgendaProjection
+import it.unibo.lss.fcla.reservation.domain.usecases.projections.MemberLedgerProjection
+import it.unibo.lss.fcla.reservation.domain.usecases.projections.OpenConsultingReservationProjection
+import it.unibo.lss.fcla.reservation.domain.usecases.projections.Projection
+import java.util.Date
+import java.util.UUID
+
+class ReservationUseCase(
+    private val agendaId: UUID,
+    private val ledgerId: UUID,
+    private val events: Map<UUID,List<Event>>) {
+
+    constructor() : this(UUID.randomUUID(),UUID.randomUUID(), mapOf())
+
+    private val eventStore: EventStore = EventStore(events)
+
+    // Fake Id used to aggregate request event
+    private val headquarterId: UUID = UUID.randomUUID()
+
+    private fun handleRequestResult(event: Event, producer: Producer): String {
+        eventStore.evolve(headquarterId, event, producer)
+        when (val resultEvent = eventStore.getStream(event.id).first()) {
+            is RequestSucceededEvent -> return resultEvent.message
+            is RequestFailedEvent -> throw RequestFailedException(resultEvent.message)
+            else -> throw RequestFailedException()
+        }
+    }
+
+    private fun <T>computeAggregate(aggregateId: UUID, projection:Projection<T>): T {
+        return eventStore.getStream(aggregateId).fold(projection.init){state,event->projection.update(state,event)}
+    }
+
+    fun requestCloseConsultingReservation(
+            reservationId: UUID,
+            memberId: UUID): String {
+        val producer: Producer = WorkoutReservationManager(agendaId, ledgerId, eventStore.get())
+        val event = CloseConsultingReservationEvent(UUID.randomUUID(), reservationId, memberId)
+        return handleRequestResult(event, producer)
+    }
+
+    fun requestCloseWorkoutReservation(
+            reservationId: UUID,
+            memberId: UUID): String {
+        val producer: Producer = WorkoutReservationManager(agendaId, ledgerId, eventStore.get())
+        val event = CloseWorkoutReservationEvent(UUID.randomUUID(), reservationId, memberId)
+        return handleRequestResult(event, producer)
+    }
+
+    fun requestCreateConsultingReservation(
+        freelancer: String,
+        date: Date,
+        firstName: String,
+        lastName: String,
+        memberId: UUID): String {
+        val producer: Producer = ConsultingReservationManager(agendaId, ledgerId, eventStore.get())
+        val event =
+            CreateConsultingReservationEvent(UUID.randomUUID(), freelancer, date, firstName, lastName, memberId)
+        return handleRequestResult(event, producer)
+    }
+
+    fun requestCreateWorkoutReservation(
+        aim: String,
+        date: Date,
+        firstName: String,
+        lastName: String,
+        memberId: UUID): String {
+        val producer: Producer = WorkoutReservationManager(agendaId, ledgerId, eventStore.get())
+        val event = CreateWorkoutReservationEvent(UUID.randomUUID(), aim, date, firstName, lastName, memberId)
+        return handleRequestResult(event, producer)
+    }
+
+    fun requestDeleteConsultingReservation(
+            reservationId: UUID,
+            memberId: UUID): String {
+        val producer: Producer = WorkoutReservationManager(agendaId, ledgerId, eventStore.get())
+        val event = DeleteConsultingReservationEvent(UUID.randomUUID(), reservationId, memberId)
+        return handleRequestResult(event, producer)
+    }
+
+    fun requestDeleteWorkoutReservation(
+            reservationId: UUID,
+            memberId: UUID): String {
+        val producer: Producer = WorkoutReservationManager(agendaId, ledgerId, eventStore.get())
+        val event = DeleteWorkoutReservationEvent(UUID.randomUUID(), reservationId, memberId)
+        return handleRequestResult(event, producer)
+    }
+
+    fun requestUpdateConsultingReservation(
+            reservationId: UUID,
+            freelancer: String,
+            date: Date): String {
+        val producer: Producer = WorkoutReservationManager(agendaId, ledgerId, eventStore.get())
+        val event = UpdateConsultingReservationEvent(UUID.randomUUID(), reservationId, freelancer, date)
+        return handleRequestResult(event, producer)
+    }
+
+    fun requestUpdateWorkoutReservation(
+            reservationId: UUID,
+            aim: String,
+            date: Date): String {
+        val producer: Producer = WorkoutReservationManager(agendaId, ledgerId, eventStore.get())
+        val event = UpdateWorkoutReservationEvent(UUID.randomUUID(), reservationId, aim, date)
+        return handleRequestResult(event, producer)
+    }
+
+    fun retrieveConsultingReservation(reservationId: UUID): ConsultingReservationFacade {
+        val agenda = computeAggregate(agendaId,AgendaProjection(agendaId))
+        when (val consultingReservation = agenda.retrieveConsultingReservation()
+                .firstOrNull { consultingReservation -> reservationId == consultingReservation.id }) {
+            is OpenConsultingReservation -> {
+                val openConsultingReservation = computeAggregate(
+                        consultingReservation.id,
+                        OpenConsultingReservationProjection(consultingReservation))
+                return ConsultingReservationFacade(
+                    openConsultingReservation.date,
+                    openConsultingReservation.freelancerId,
+                    openConsultingReservation.id,
+                    isOpen = true)
+            }
+            is CloseConsultingReservation -> {
+                return ConsultingReservationFacade(
+                    consultingReservation.date,
+                    consultingReservation.freelancerId,
+                    consultingReservation.id,
+                    isOpen = false)
+            }
+            else -> {
+                // TODO Need tests and then remove
+                if (consultingReservation != null) {
+                    // This should not happen
+                    throw RequestFailedException()
+                }
+                throw RequestFailedException(RequestFailedMessages.reservationNotFound)
+            }
+        }
+    }
+}
