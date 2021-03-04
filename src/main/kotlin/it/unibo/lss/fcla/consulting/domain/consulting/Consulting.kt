856d5fdd15c9450d89df1ca61ccc2ca9e5469cd2
Stefano <stefano.braggion@me.com>
2021-03-04 12:25:46 +0100

    refactoring consulting. consulting tests green

--- src/main/kotlin/it/unibo/lss/fcla/consulting/domain/consulting/Consulting.kt
+++ src/main/kotlin/it/unibo/lss/fcla/consulting/domain/consulting/Consulting.kt
@@ -15,39 +15,21 @@ typealias ConsultingId = String
  * Representing a consulting
  */
 class Consulting(
-    val consultingId: ConsultingId
+    val consultingId: ConsultingId,
+    val freelancerId: FreelancerId
 ) : AbstractAggregate(consultingId) {
 
     private lateinit var consultingSummary: ConsultingSummary
 
-    constructor(
-        consultingId: ConsultingId,
-        consultingType: String,
-        description: String,
-        consultingDate: Date,
-        freelancerId: FreelancerId
-    ) :
-        this(consultingId) {
-            this.raiseEvent(
-                ConsultingSummaryCreatedEvent(
-                    consultingId,
-                    consultingType,
-                    consultingDate,
-                    description,
-                    freelancerId
-                )
-            )
-        }
 
     companion object {
-        fun createConsulting(consultingId: ConsultingId, consultingType: String,
-            description: String, consultingDate: Date, freelancerId: FreelancerId) : Consulting {
+        fun createConsulting(consultingId: ConsultingId, freelancerId: FreelancerId) : Consulting {
 
-            return Consulting(consultingId, consultingType, description, consultingDate, freelancerId)
+            return Consulting(consultingId, freelancerId)
         }
 
-        fun hydrateConsulting(aggregateId: AggregateId, eventList: List<DomainEvent>) : Consulting {
-            var consulting = Consulting(aggregateId);
+        fun hydrateConsulting(aggregateId: AggregateId, freelancerId: FreelancerId, eventList: List<DomainEvent>) : Consulting {
+            var consulting = Consulting.createConsulting(aggregateId, freelancerId)
             eventList.forEach { consulting.applyEvent(it) }
 
             return consulting
@@ -80,6 +62,7 @@ class Consulting(
 
     /**
      * Apply the event: created a new consulting summary
+     * TODO add validation for data
      */
     private fun apply(event: ConsultingSummaryCreatedEvent) {
         if (event.description.isEmpty()) throw ConsultingSummaryDescriptionCannotBeEmpty()
@@ -91,7 +74,6 @@ class Consulting(
     override fun applyEvent(event: DomainEvent) {
         when (event) {
             is ConsultingSummaryUpdatedDescriptionEvent -> apply(event)
-            is ConsultingSummaryCreatedEvent -> apply(event)
             else -> throw IllegalArgumentException() //TODO fixme
         }
     }
