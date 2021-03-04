7f378cf13d7aa63f7dcba6467e45aec24d222ffa
alessia <alessia.cerami@studio.unibo.it>
2021-03-04 12:41:13 +0100

    added error request message

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/events/results/RequestFailedMessages.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/events/results/RequestFailedMessages.kt
@@ -3,4 +3,5 @@ package it.unibo.lss.fcla.reservation.domain.usecases.events.results
 object RequestFailedMessages {
     const val reservationNotFound = "The requested reservation was not found."
     const val emptyWorkoutAim = "Unable to create a workout reservation due to empty aim."
+    const val pastDateInReservation = "Unable to create a consulting due to insertion of a past date."
 }
