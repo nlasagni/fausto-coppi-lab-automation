c0b7123070278cad699a9ef80036dc10cb893c4d
Andrea <andrea.giordano5@studio.unibo.it>
2021-03-05 16:41:54 +0100

    Added ReservationDate facades

---
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/facades/ConsultingReservationDateFacade.kt
@@ -0,0 +1,6 @@
+package it.unibo.lss.fcla.reservation.domain.usecases.facades
+
+import java.util.Date
+import java.util.UUID
+
+class ConsultingReservationDateFacade(val reservationId: UUID, val date: Date)
