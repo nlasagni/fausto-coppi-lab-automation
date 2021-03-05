6392985ae75f66e91c4d3824c1bfd9d21b242b96
Andrea <andrea.giordano5@studio.unibo.it>
2021-03-05 16:42:09 +0100

    Added fail message

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/events/results/RequestFailedMessages.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/events/results/RequestFailedMessages.kt
@@ -4,5 +4,6 @@ object RequestFailedMessages {
     const val reservationNotFound = "The requested reservation was not found."
     const val emptyWorkoutAim = "Unable to create a workout reservation due to empty aim."
     const val emptyConsultingFreelancer = "Unable to create a consulting reservation due to empty freelancer."
-    const val pastDateInReservation = "Unable to create a consulting due to insertion of a past date."
+    const val pastDateInReservation = "Unable to create a reservation due to insertion of a past date."
+    const val noUpdateToCloseReservation = "Update to close reservation are not allowed."
 }
