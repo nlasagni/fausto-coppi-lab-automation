93b36963b2048b9327d4b1a43c6761fa18352692
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-03-06 12:42:55 +0100

    Adds basic AthleticPreparationRepository interface method

--- src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/usecase/port/AthleticPreparationRepository.kt
+++ src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/usecase/port/AthleticPreparationRepository.kt
@@ -1,7 +1,16 @@
 package it.unibo.lss.fcla.athleticpreparation.usecase.port
 
+import it.unibo.lss.fcla.athleticpreparation.domain.model.AthleticPreparation
+
 /**
  * @author Nicola Lasagni on 06/03/2021.
  */
 interface AthleticPreparationRepository {
+
+    fun create(athleticPreparation: AthleticPreparation): AthleticPreparation
+
+    fun findById(id: String): AthleticPreparation?
+
+    fun findByMemberId(memberId: String): List<AthleticPreparation>?
+
 }
