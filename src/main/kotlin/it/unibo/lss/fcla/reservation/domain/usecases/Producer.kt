9c28ad7b27a70aedee67ebdbcbca8c01a757cbd5
alessia <alessia.cerami@studio.unibo.it>
2021-03-06 13:07:55 +0100

    added javadoc into producer interface

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/Producer.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/Producer.kt
@@ -3,6 +3,14 @@ package it.unibo.lss.fcla.reservation.domain.usecases
 import it.unibo.lss.fcla.reservation.common.Event
 import java.util.UUID
 
+/**
+ * The producer of events which will be stored into the vent Store
+ */
 interface Producer {
+
+    /**
+     * Returns a [Map] with the [UUID] of the aggregate as key and a [List] of [Event] as value for
+     * the aggregate.
+     */
     fun produce(event: Event): Map<UUID, List<Event>>
 }
