7c8ecfd7d9189132de502617942efcfad6ac3dc5
alessia <alessia.cerami@studio.unibo.it>
2021-03-06 13:08:14 +0100

    added javadoc into requestFailedException

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/RequestFailedException.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/RequestFailedException.kt
@@ -1,3 +1,7 @@
 package it.unibo.lss.fcla.reservation.domain.usecases
 
+/**
+ * Throws [RequestFailedException] is an error occurred during a request of a reservation
+ * showing the error [message].
+ */
 class RequestFailedException(message: String = "Something really bad happened") : Exception(message)
