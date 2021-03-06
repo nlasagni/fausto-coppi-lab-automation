eca1a1b496f648874576d172461e0bbcb4afe00c
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-03-06 14:26:29 +0100

    Introduces identity classes in repository

--- src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/usecase/port/AthleticPreparationRepository.kt
+++ src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/usecase/port/AthleticPreparationRepository.kt
@@ -1,6 +1,7 @@
 package it.unibo.lss.fcla.athleticpreparation.usecase.port
 
 import it.unibo.lss.fcla.athleticpreparation.domain.model.AthleticPreparation
+import it.unibo.lss.fcla.athleticpreparation.domain.model.AthleticPreparationId
 
 /**
  * @author Nicola Lasagni on 06/03/2021.
@@ -9,7 +10,7 @@ interface AthleticPreparationRepository {
 
     fun create(athleticPreparation: AthleticPreparation): AthleticPreparation
 
-    fun findById(id: String): AthleticPreparation?
+    fun findById(id: AthleticPreparationId): AthleticPreparation?
 
     fun findByMemberId(memberId: String): List<AthleticPreparation>?
 
