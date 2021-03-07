8de79ace34648bce19aa1d9dce0e9b0d50b62986
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-03-07 19:53:12 +0100

    Fixes AthleticPreparationRepository interface

--- src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/usecase/port/AthleticPreparationRepository.kt
+++ src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/usecase/port/AthleticPreparationRepository.kt
@@ -2,16 +2,21 @@ package it.unibo.lss.fcla.athleticpreparation.usecase.port
 
 import it.unibo.lss.fcla.athleticpreparation.domain.model.AthleticPreparation
 import it.unibo.lss.fcla.athleticpreparation.domain.model.AthleticPreparationId
+import it.unibo.lss.fcla.athleticpreparation.domain.model.PeriodOfPreparation
 
 /**
  * @author Nicola Lasagni on 06/03/2021.
  */
 interface AthleticPreparationRepository {
 
-    fun create(athleticPreparation: AthleticPreparation): AthleticPreparation
+    fun add(athleticPreparation: AthleticPreparation): AthleticPreparation
 
     fun findById(id: AthleticPreparationId): AthleticPreparation?
 
-    fun findByMemberId(memberId: String): List<AthleticPreparation>?
+    fun findAllByAthleticTrainerId(athleticTrainerId: String): List<AthleticPreparation>
+
+    fun findAllByMemberId(memberId: String): List<AthleticPreparation>
+
+    fun findAllByPeriodOfPreparation(periodOfPreparation: PeriodOfPreparation): List<AthleticPreparation>
 
 }
