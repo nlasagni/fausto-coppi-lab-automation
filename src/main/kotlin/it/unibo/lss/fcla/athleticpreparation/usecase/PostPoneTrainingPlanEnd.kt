092bb1b44709db98d666490a06d26130305b1080
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-03-07 16:05:42 +0100

    Removes other repository and improves use cases

---
+++ src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/usecase/PostPoneTrainingPlanEnd.kt
@@ -0,0 +1,7 @@
+package it.unibo.lss.fcla.athleticpreparation.usecase
+
+/**
+ * @author Nicola Lasagni on 07/03/2021.
+ */
+class PostPoneTrainingPlanEnd {
+}
--- src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/usecase/ScheduleWorkout.kt
+++ src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/usecase/ScheduleWorkout.kt
@@ -3,5 +3,5 @@ package it.unibo.lss.fcla.athleticpreparation.usecase
 /**
  * @author Nicola Lasagni on 06/03/2021.
  */
-class CreateWorkout {
+class ScheduleWorkout {
 }
