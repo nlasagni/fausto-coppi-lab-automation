1e0ce8319f886355adf0074343f9ad3ca0eda96a
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-03-07 19:53:53 +0100

    Adds InMemoryAthleticPreparationRepository implementation and related test

---
+++ src/main/kotlin/it/unibo/lss/fcla/athleticpreparation/adapter/repository/InMemoryAthleticPreparationRepository.kt
@@ -0,0 +1,35 @@
+package it.unibo.lss.fcla.athleticpreparation.adapter.repository
+
+import it.unibo.lss.fcla.athleticpreparation.domain.model.AthleticPreparation
+import it.unibo.lss.fcla.athleticpreparation.domain.model.AthleticPreparationId
+import it.unibo.lss.fcla.athleticpreparation.domain.model.PeriodOfPreparation
+import it.unibo.lss.fcla.athleticpreparation.usecase.port.AthleticPreparationRepository
+
+/**
+ * @author Nicola Lasagni on 07/03/2021.
+ */
+class InMemoryAthleticPreparationRepository : AthleticPreparationRepository {
+
+    private val inMemoryStorage: MutableMap<AthleticPreparationId, AthleticPreparation> = mutableMapOf()
+
+    override fun add(athleticPreparation: AthleticPreparation): AthleticPreparation {
+        val id = athleticPreparation.snapshot().id
+        inMemoryStorage[id] = athleticPreparation
+        return athleticPreparation
+    }
+
+    override fun findById(id: AthleticPreparationId): AthleticPreparation? = inMemoryStorage[id]
+
+    override fun findAllByAthleticTrainerId(athleticTrainerId: String): List<AthleticPreparation> {
+        return inMemoryStorage.values.filter { it.snapshot().athleticTrainerId == athleticTrainerId }
+    }
+
+    override fun findAllByMemberId(memberId: String): List<AthleticPreparation> {
+        return inMemoryStorage.values.filter { it.snapshot().memberId == memberId }
+    }
+
+    override fun findAllByPeriodOfPreparation(periodOfPreparation: PeriodOfPreparation): List<AthleticPreparation> {
+        return inMemoryStorage.values.filter { it.snapshot().periodOfPreparation == periodOfPreparation }
+    }
+
+}
