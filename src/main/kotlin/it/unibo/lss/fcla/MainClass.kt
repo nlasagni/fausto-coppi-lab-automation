0cdaa04fad852ffc5e80c57663435c0ebfcfa613
Andrea <andrea.giordano5@studio.unibo.it>
2021-03-09 22:56:45 +0100

    Basic main class

--- src/main/kotlin/it/unibo/lss/fcla/MainClass.kt
+++ src/main/kotlin/it/unibo/lss/fcla/MainClass.kt
@@ -1,45 +1,48 @@
 package it.unibo.lss.fcla
 
+import it.unibo.lss.fcla.reservation.domain.usecases.CommandReservationUseCase
+import it.unibo.lss.fcla.reservation.domain.usecases.EventStore
+import it.unibo.lss.fcla.reservation.domain.usecases.QueryReservationUseCase
+import java.util.Calendar
+import java.util.UUID
+
 class MainClass {
-
-    /* companion object MainClass {
-        const val YEAR = 2021
-        const val DAY = 25
-        const val HOUR = 11
-        const val MINUTE = 15
-        val cal: Calendar = Calendar.getInstance()
-        private const val cardCode = "2025"
-        val member = Member(
-            "Mario",
-            "Rossi",
-            MembershipCard(cardCode)
-        )
-        val machine = Machine("Leg press")
-        val professional = Professional(
-            ProfessionalType.ATHLETIC_TRAINER,
-            "Mimmo",
-            "Bianchi"
-        )
-        val desk = Desk()
+    companion object {
+        private const val year = 2021
+        private const val day = 13
+        private val agendaId = UUID.randomUUID()
+        private val ledgerId = UUID.randomUUID()
+        private val eventStore = EventStore()
+        private const val freelancer = "Physiotherapist"
+        private const val month = Calendar.APRIL
+        private val calendar = Calendar.getInstance()
+        private const val memberFirstName = "Mario"
+        private const val memberLastName = "Rossi"
+        private val memberId = UUID.randomUUID()
     }
-
-    fun basicGymReservation() {
-        cal.set(YEAR, Calendar.FEBRUARY, DAY, HOUR, MINUTE)
-        desk.createGymReservation(cal.time, member, machine)
-        println("Reservation created: ${desk.read()}")
+    fun createReservation() {
+        calendar.set(year, month, day)
+        val reservationDate = calendar.time
+        val commandReservationUseCase = CommandReservationUseCase(agendaId, ledgerId, eventStore)
+        val queryReservationUseCase = QueryReservationUseCase(agendaId, ledgerId, eventStore)
+        println(
+            "Result of the request: " + commandReservationUseCase.requestCreateConsultingReservation(
+                freelancer,
+                reservationDate,
+                memberFirstName,
+                memberLastName,
+                memberId
+            )
+        )
+        val retrievedReservation =
+            queryReservationUseCase.retrieveMemberConsultingReservations(memberId).first()
+        println("Retrieved reservation: $retrievedReservation")
+        val reservationDetails =
+            queryReservationUseCase.retrieveConsultingReservation(retrievedReservation.reservationId)
+        println("Reservation details: $reservationDetails")
     }
-
-    fun basicConsultingReservation() {
-        cal.set(YEAR, Calendar.MARCH, DAY, HOUR, MINUTE)
-        desk.createConsultingReservation(cal.time, member, professional, Consulting.ATHLETIC_TRAINING)
-        println("Reservation created: ${desk.read()}")
-    } */
 }
 
 fun main() {
-    /*
-    MainClass().basicGymReservation()
-    MainClass().basicConsultingReservation()
-    */
-    println("Main Class")
+    MainClass().createReservation()
 }
