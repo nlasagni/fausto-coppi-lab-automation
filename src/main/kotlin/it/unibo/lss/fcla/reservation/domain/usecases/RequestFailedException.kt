e9078a11cf78047668258a61bbfe354f80bfba62
alessia <alessia.cerami@studio.unibo.it>
2021-03-09 22:14:09 +0100

    refactored exception message

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/RequestFailedException.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/usecases/RequestFailedException.kt
@@ -4,4 +4,5 @@ package it.unibo.lss.fcla.reservation.domain.usecases
  * Throws [RequestFailedException] is an error occurred during a request of a reservation
  * showing the error [message].
  */
-class RequestFailedException(message: String = "Something really bad happened") : Exception(message)
+class RequestFailedException(message: String = "The request you have done encountered a problem! Try it later") :
+    Exception(message)
