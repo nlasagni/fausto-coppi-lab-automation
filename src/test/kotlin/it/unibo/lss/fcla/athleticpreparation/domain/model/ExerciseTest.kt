a7443aa7469f1a6cf7829417327982c7e32fc42b
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-03-11 17:44:52 +0100

    Removes empty ExerciseTest class

--- src/test/kotlin/it/unibo/lss/fcla/athleticpreparation/domain/model/ExerciseTest.kt
+++
@@ -1,9 +0,0 @@
-package it.unibo.lss.fcla.athleticpreparation.domain.model
-
-import io.kotest.core.spec.style.FreeSpec
-
-/**
- * @author Nicola Lasagni on 04/03/2021.
- */
-class ExerciseTest : FreeSpec({
-})
