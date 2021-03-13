2f346c99cb7747de947136dd60a682198787fd8c
alessia <alessia.cerami@studio.unibo.it>
2021-03-13 17:15:43 +0100

    renamed and implemented repository class

---
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/persistence/RepositoryInMemory.kt
@@ -0,0 +1,28 @@
+package it.unibo.lss.fcla.reservation.persistence
+
+import it.unibo.lss.fcla.reservation.common.Event
+import it.unibo.lss.fcla.reservation.domain.usecases.Repository
+import java.util.UUID
+
+/**
+ * Class used to access to data stored into data layer.
+ *
+ * This class implements the interface [Repository] exposed by the use case layer.
+ */
+class RepositoryInMemory: Repository{
+    private var eventsInMemoryMap: Map<UUID, List<Event>> = mapOf()
+
+    /**
+     * Method used to store events in memory
+     */
+    override fun writeEvents(events: Map<UUID, List<Event>>) {
+        eventsInMemoryMap = events
+    }
+
+    /**
+     * Method that returns a map of the stored events which are associated to an aggregate id
+     */
+    override fun readEvents(): Map<UUID, List<Event>> {
+        return eventsInMemoryMap.toMap()
+    }
+}
