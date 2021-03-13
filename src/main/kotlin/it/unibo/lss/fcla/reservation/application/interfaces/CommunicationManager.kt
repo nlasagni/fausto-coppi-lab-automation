3cbe60c0beadffebc7f5d4d38b99ce698b33e83f
alessia <alessia.cerami@studio.unibo.it>
2021-03-13 17:16:21 +0100

    fixed return type of the pickRequest function

--- src/main/kotlin/it/unibo/lss/fcla/reservation/application/interfaces/CommunicationManager.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/application/interfaces/CommunicationManager.kt
@@ -2,7 +2,7 @@ package it.unibo.lss.fcla.reservation.application.interfaces
 
 interface CommunicationManager {
 
-    fun pickRequest(): String
+    fun pickRequest(): String?
 
     fun publishResult(result: String)
 }
