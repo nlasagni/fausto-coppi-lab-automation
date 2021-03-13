eef092f52c60f07b0bc91c7851ce513f759a7551
alessia <alessia.cerami@studio.unibo.it>
2021-03-13 17:16:40 +0100

    implemented communication manager class

--- src/main/kotlin/it/unibo/lss/fcla/reservation/infrastructure/CommunicationManagerImpl.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/infrastructure/CommunicationManagerImpl.kt
@@ -1,3 +1,27 @@
 package it.unibo.lss.fcla.reservation.infrastructure
 
-class CommunicationManagerImpl
+import it.unibo.lss.fcla.reservation.application.interfaces.CommunicationManager
+
+/**
+ * Class to handle the means of communication.
+ *
+ * It implements the [CommunicationManager] interface exposed by application layer.
+ */
+class CommunicationManagerImpl: CommunicationManager {
+    private val events: ArrayDeque<String> = ArrayDeque()
+
+    /**
+     * Method used to pick from list the first event and returns a string which represents the request.
+     */
+    override fun pickRequest(): String? {
+        return events.removeFirstOrNull()
+    }
+
+    /**
+     * Method used to publish into the EventBus the request
+     */
+    override fun publishResult(result: String) {
+        println("$result published into the Bus!")
+    }
+
+}
