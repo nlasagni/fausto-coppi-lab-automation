7c9d814cdfda3d5f65015cc3f5e3875980200914
Andrea <andrea.giordano5@studio.unibo.it>
2021-03-15 00:27:01 +0100

    Main class launch Controller

--- src/main/kotlin/it/unibo/lss/fcla/MainClass.kt
+++ src/main/kotlin/it/unibo/lss/fcla/MainClass.kt
@@ -1,5 +1,6 @@
 package it.unibo.lss.fcla
 
+import it.unibo.lss.fcla.reservation.application.Controller
 import it.unibo.lss.fcla.reservation.domain.usecases.CommandReservationUseCase
 import it.unibo.lss.fcla.reservation.domain.usecases.EventStore
 import it.unibo.lss.fcla.reservation.domain.usecases.QueryReservationUseCase
@@ -7,42 +8,11 @@ import java.util.Calendar
 import java.util.UUID
 
 class MainClass {
-    companion object {
-        private const val year = 2021
-        private const val day = 13
-        private val agendaId = UUID.randomUUID()
-        private val ledgerId = UUID.randomUUID()
-        private val eventStore = EventStore()
-        private val freelancer = UUID.randomUUID()
-        private const val month = Calendar.APRIL
-        private val calendar = Calendar.getInstance()
-        private const val memberFirstName = "Mario"
-        private const val memberLastName = "Rossi"
-        private val memberId = UUID.randomUUID()
-    }
-    fun createReservation() {
-        calendar.set(year, month, day)
-        val reservationDate = calendar.time
-        val commandReservationUseCase = CommandReservationUseCase(agendaId, ledgerId, eventStore)
-        val queryReservationUseCase = QueryReservationUseCase(agendaId, ledgerId, eventStore)
-        println(
-            "Result of the request: " + commandReservationUseCase.requestCreateConsultingReservation(
-                freelancer,
-                reservationDate,
-                memberFirstName,
-                memberLastName,
-                memberId
-            )
-        )
-        val retrievedReservation =
-            queryReservationUseCase.retrieveMemberConsultingReservations(memberId).first()
-        println("Retrieved reservation: $retrievedReservation")
-        val reservationDetails =
-            queryReservationUseCase.retrieveConsultingReservation(retrievedReservation.reservationId)
-        println("Reservation details: $reservationDetails")
+    fun launchReservationMicroservice() {
+        Controller().start()
     }
 }
 
 fun main() {
-    MainClass().createReservation()
+    MainClass().launchReservationMicroservice()
 }
