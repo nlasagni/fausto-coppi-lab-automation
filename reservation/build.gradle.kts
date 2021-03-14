be90215bd648e455a1ba27a4e56e823102ad522c
Stefano <stefano.braggion@me.com>
2021-03-14 12:02:31 +0100

    removed duplicated code from reservation build.gradle.kts

--- reservation/build.gradle.kts
+++ reservation/build.gradle.kts
@@ -1,25 +1,5 @@
 val myMainClass = "it.unibo.lss.fcla.MainClassKt"
 
-plugins {
-    kotlin("jvm")
-    id("org.danilopianini.git-sensitive-semantic-versioning")
-    id("pl.droidsonroids.jacoco.testkit")
-    id("org.jlleitschuh.gradle.ktlint")
-    id("io.gitlab.arturbosch.detekt")
-    id("org.jetbrains.dokka")
-    jacoco
-}
-
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.50")
-    testImplementation(gradleTestKit())
-    testImplementation("io.kotest:kotest-runner-junit5:4.2.5")
-    testImplementation("io.kotest:kotest-assertions-core:4.2.5")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:4.2.5")
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.14.1")
-}
-
 detekt {
     failFast = true
     buildUponDefaultConfig = true
