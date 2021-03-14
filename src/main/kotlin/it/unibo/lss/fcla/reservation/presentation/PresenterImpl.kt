781e2b91945979b5b2960c682ebf78532ef13f73
Andrea <andrea.giordano5@studio.unibo.it>
2021-03-14 16:15:14 +0100

    Added missing javadoc to PresenterImpl

--- src/main/kotlin/it/unibo/lss/fcla/reservation/presentation/PresenterImpl.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/presentation/PresenterImpl.kt
@@ -2,20 +2,26 @@ package it.unibo.lss.fcla.reservation.presentation
 
 import it.unibo.lss.fcla.reservation.application.interfaces.Presenter
 
+/**
+ * Implementation of [Request] interface
+ *
+ * This class handle the conversion logic from and to [String].
+ * It is used by application layer to convert messages handled by infrastructure layer.
+ *
+ */
 class PresenterImpl : Presenter {
 
+    /**
+     * convert a [String] into the corresponding [Request]
+     */
     override fun convertRequest(request: String): Request {
         TODO()
-        /*
-        when (request) {
-            "RequestRetrieveAgendaConsultingReservation" -> RequestRetrieveAgendaConsultingReservation()
-            "RequestRetrieveAgendaWorkoutReservation" -> RequestRetrieveAgendaWorkoutReservation()
-            else -> RequestRetrieveAgendaConsultingReservation()
-        }
-        */
     }
 
+    /**
+     * convert a [Result] into the corresponding [String]
+     */
     override fun convertResult(result: Result): String {
-        return result.toString()
+        TODO()
     }
 }
