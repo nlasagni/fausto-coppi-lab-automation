ca69642bd0b2908e65570933bedb52832ed410eb
Andrea <andrea.giordano5@studio.unibo.it>
2021-03-15 00:51:41 +0100

    Fixed lint style, added companion object for ConsoleUI

--- src/main/kotlin/it/unibo/lss/fcla/reservation/ui/ConsoleUI.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/ui/ConsoleUI.kt
@@ -19,74 +19,89 @@ class ConsoleUI {
     private val aim2 = "Strengthening"
 
     init {
-        cal.set(2021,Calendar.APRIL,7)
+        cal.set(year, Calendar.APRIL, day7)
         date7April = cal.time
-        cal.set(2021,Calendar.APRIL,24)
+        cal.set(year, Calendar.APRIL, day24)
         date24April = cal.time
     }
 
-    private fun writeAllCommands() {
-        println("List of possible commands\n" +
-                "1) Create Consulting Reservation\n" +
-                "2) Create Workout Reservation\n" +
-                "3) Update Consulting Reservation\n" +
-                "4) Update Workout Reservation\n" +
-                "5) Delete Consulting Reservation\n" +
-                "6) Delete Workout Reservation\n" +
-                "7) Consulting Reservation Details\n" +
-                "8) Workout Reservation Details\n" +
-                "9) Close and exit")
-    }
+    companion object {
+        private const val year = 2021
+        private const val day7 = 7
+        private const val day24 = 24
+        private const val numberOfCommands = 9
 
-    private fun readInput(max: Int): Int {
-        var inputRead = false
-        var validInput = 0
-        do {
-            val str = readLine() ?: ""
-            try {
-                validInput = str.toInt()
-                if(validInput in 1..max) {
-                    inputRead = true
-                } else {
+        private fun writeAllCommands() {
+            println(
+                "List of possible commands\n" +
+                    "1) Create Consulting Reservation\n" +
+                    "2) Create Workout Reservation\n" +
+                    "3) Update Consulting Reservation\n" +
+                    "4) Update Workout Reservation\n" +
+                    "5) Delete Consulting Reservation\n" +
+                    "6) Delete Workout Reservation\n" +
+                    "7) Consulting Reservation Details\n" +
+                    "8) Workout Reservation Details\n" +
+                    "9) Close and exit"
+            )
+        }
+
+        private fun readInput(max: Int): Int {
+            var inputRead = false
+            var validInput = 0
+            do {
+                val str = readLine() ?: ""
+                try {
+                    validInput = str.toInt()
+                    if (validInput in 1..max) {
+                        inputRead = true
+                    } else {
+                        println("Please insert a valid number")
+                    }
+                } catch (exception: NumberFormatException) {
                     println("Please insert a valid number")
                 }
-            } catch (exception: NumberFormatException) {
-                println("Please insert a valid number")
-            }
-        } while (! inputRead)
-        return validInput
+            } while (! inputRead)
+            return validInput
+        }
     }
 
     fun readCommand(): Int {
         writeAllCommands()
-        return readInput(9)
+        return readInput(numberOfCommands)
     }
 
     fun readFreelancer(): UUID {
-        println("Choose a freelancer\n" +
+        println(
+            "Choose a freelancer\n" +
                 "1) Mario Rossi\n" +
-                "2) Giovanna Bianchi\n")
-        return when(readInput(2)){
+                "2) Giovanna Bianchi\n"
+        )
+        return when (readInput(2)) {
             1 -> freelancer1
             else -> freelancer2
         }
     }
 
     fun readDate(): Date {
-        println("Choose a date\n" +
+        println(
+            "Choose a date\n" +
                 "1) 2021/4/7\n" +
-                "2) 2021/4/24\n")
-        return when(readInput(2)){
+                "2) 2021/4/24\n"
+        )
+        return when (readInput(2)) {
             1 -> date7April
             else -> date24April
         }
     }
 
     fun readAim(): String {
-        println("Choose an aim\n" +
+        println(
+            "Choose an aim\n" +
                 "1) Injury recovery\n" +
-                "2) Strengthening\n")
-        return when(readInput(2)){
+                "2) Strengthening\n"
+        )
+        return when (readInput(2)) {
             1 -> aim1
             else -> aim2
         }
@@ -117,18 +132,22 @@ class ConsoleUI {
     }
 
     fun printConsultingReservationDetails(res: ConsultingReservationFacade) {
-        println("Consulting reservation details: \n" +
+        println(
+            "Consulting reservation details: \n" +
                 "ReservationId: ${res.id}\n" +
                 "Date: ${res.date}\n" +
                 "FreelancerId: ${res.freelancerId}\n" +
-                "IsOpen: ${res.isOpen}")
+                "IsOpen: ${res.isOpen}"
+        )
     }
 
     fun printWorkoutReservationDetails(res: WorkoutReservationFacade) {
-        println("Consulting reservation details: \n" +
+        println(
+            "Consulting reservation details: \n" +
                 "ReservationId: ${res.id}\n" +
                 "Date: ${res.date}\n" +
                 "Aim: ${res.aim}\n" +
-                "IsOpen: ${res.isOpen}")
+                "IsOpen: ${res.isOpen}"
+        )
     }
 }
