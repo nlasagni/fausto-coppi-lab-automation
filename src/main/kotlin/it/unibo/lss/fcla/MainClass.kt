5627482edee7c5d00df7122a40802b307c4075e0
Andrea <andrea.giordano5@studio.unibo.it>
2021-03-15 00:52:00 +0100

    Fixed lint style to MainClass

--- src/main/kotlin/it/unibo/lss/fcla/MainClass.kt
+++ src/main/kotlin/it/unibo/lss/fcla/MainClass.kt
@@ -1,11 +1,6 @@
 package it.unibo.lss.fcla
 
 import it.unibo.lss.fcla.reservation.application.Controller
-import it.unibo.lss.fcla.reservation.domain.usecases.CommandReservationUseCase
-import it.unibo.lss.fcla.reservation.domain.usecases.EventStore
-import it.unibo.lss.fcla.reservation.domain.usecases.QueryReservationUseCase
-import java.util.Calendar
-import java.util.UUID
 
 class MainClass {
     fun launchReservationMicroservice() {
