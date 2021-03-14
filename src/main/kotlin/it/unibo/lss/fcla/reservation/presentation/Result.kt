b968272044675f5f5867e88dd7ae726ad43cb91b
Andrea <andrea.giordano5@studio.unibo.it>
2021-03-14 16:03:45 +0100

    Added Result implementations

--- src/main/kotlin/it/unibo/lss/fcla/reservation/presentation/Result.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/presentation/Result.kt
@@ -1,3 +1,38 @@
 package it.unibo.lss.fcla.reservation.presentation
 
+import it.unibo.lss.fcla.reservation.domain.usecases.facades.ConsultingReservationDateFacade
+import it.unibo.lss.fcla.reservation.domain.usecases.facades.ConsultingReservationFacade
+import it.unibo.lss.fcla.reservation.domain.usecases.facades.WorkoutReservationDateFacade
+import it.unibo.lss.fcla.reservation.domain.usecases.facades.WorkoutReservationFacade
+
+/**
+ * Interface used to group all result type
+ */
 interface Result
+
+/**
+ * Specific implementation of [Result] for a [Request] containing a [message]
+ */
+class ResultRequestMessage(val message: String) : Result
+
+/**
+ * Specific implementation of [Result] for a retrieve [Request] containing a [WorkoutReservationDateFacade] list
+ */
+class ResultRetrieveWorkoutReservationList(val workoutReservations: List<WorkoutReservationDateFacade>) : Result
+
+/**
+ * Specific implementation of [Result] for a retrieve [Request] containing a [ConsultingReservationDateFacade] list
+ */
+class ResultRetrieveConsultingReservationList(
+    val consultingReservations: List<ConsultingReservationDateFacade>
+) : Result
+
+/**
+ * Specific implementation of [Result] for a retrieve [Request] containing a [WorkoutReservationFacade]
+ */
+class ResultRetrieveWorkoutReservation(val workoutReservation: WorkoutReservationFacade) : Result
+
+/**
+ * Specific implementation of [Result] for a retrieve [Request] containing a [ConsultingReservationFacade]
+ */
+class ResultRetrieveConsultingReservation(val consultingReservation: ConsultingReservationFacade) : Result
