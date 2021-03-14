f8023f85a09516bac9613472d5104c0ee93e1afc
alessia <alessia.cerami@studio.unibo.it>
2021-03-14 17:41:02 +0100

    added aim value object

---
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/reservation/Aim.kt
@@ -0,0 +1,11 @@
+package it.unibo.lss.fcla.reservation.domain.entities.reservation
+
+import it.unibo.lss.fcla.reservation.domain.entities.exceptions.WorkoutReservationAimCannotBeEmpty
+
+data class Aim(val aim: String) {
+    init{
+        if (aim.isEmpty()) {
+            throw WorkoutReservationAimCannotBeEmpty()
+        }
+    }
+}
