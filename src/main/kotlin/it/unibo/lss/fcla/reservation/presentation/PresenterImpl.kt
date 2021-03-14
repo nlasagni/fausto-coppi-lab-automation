77b43f0b845279d90801fe1d3e4105f7fbcec739
Andrea <andrea.giordano5@studio.unibo.it>
2021-03-14 16:05:34 +0100

    Added implementation of PresenterImpl

--- src/main/kotlin/it/unibo/lss/fcla/reservation/presentation/PresenterImpl.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/presentation/PresenterImpl.kt
@@ -1,3 +1,21 @@
 package it.unibo.lss.fcla.reservation.presentation
 
-class PresenterImpl
+import it.unibo.lss.fcla.reservation.application.interfaces.Presenter
+
+class PresenterImpl: Presenter {
+
+    override fun convertRequest(request: String): Request {
+        TODO()
+        /*
+        when (request) {
+            "RequestRetrieveAgendaConsultingReservation" -> RequestRetrieveAgendaConsultingReservation()
+            "RequestRetrieveAgendaWorkoutReservation" -> RequestRetrieveAgendaWorkoutReservation()
+            else -> RequestRetrieveAgendaConsultingReservation()
+        }
+        */
+    }
+
+    override fun convertResult(result: Result): String {
+        return result.toString()
+    }
+}
