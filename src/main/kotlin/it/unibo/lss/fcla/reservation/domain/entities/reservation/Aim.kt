1139213947aa568959cd8f82c0404f297c9221e2
alessia <alessia.cerami@studio.unibo.it>
2021-03-15 11:20:55 +0100

    added missing javadoc into value object classes

--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/reservation/Aim.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/reservation/Aim.kt
@@ -2,7 +2,16 @@ package it.unibo.lss.fcla.reservation.domain.entities.reservation
 
 import it.unibo.lss.fcla.reservation.domain.entities.exceptions.WorkoutReservationAimCannotBeEmpty
 
+/**
+ * Aim value object.
+ *
+ * It is the unique identifier of a workout [aim]
+ */
 data class Aim(val aim: String) {
+
+    /**
+     * check invariant
+     */
     init {
         if (aim.isEmpty()) {
             throw WorkoutReservationAimCannotBeEmpty()
--- src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/reservation/FreelancerId.kt
+++ src/main/kotlin/it/unibo/lss/fcla/reservation/domain/entities/reservation/FreelancerId.kt
@@ -3,7 +3,16 @@ package it.unibo.lss.fcla.reservation.domain.entities.reservation
 import it.unibo.lss.fcla.reservation.domain.entities.exceptions.ConsultingReservationFreelancerCannotBeEmpty
 import java.util.UUID
 
+/**
+ * Freelancer value object.
+ *
+ * It is the unique identifier of a Freelancer identified by its [freelancerId]
+ */
 class FreelancerId(val freelancerId: UUID) {
+
+    /**
+     * check invariant
+     */
     init {
         if (freelancerId == UUID(0, 0)) {
             throw ConsultingReservationFreelancerCannotBeEmpty()
