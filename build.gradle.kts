62d334d1b85af00726f1a6fe0db9eb565804b399
Stefano <stefano.braggion@me.com>
2021-03-18 11:17:37 +0100

    updated comments in build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,31 +31,25 @@ subprojects {
 
     afterEvaluate {
         tasks.jar {
-            // Otherwise it throws a "No main manifest attribute" error
-
             val mainClassVarName = "mainclass"
 
             manifest {
                 attributes(
+                    // Otherwise it throws a "No main manifest attribute" error
                     mapOf(
                         "Main-Class" to ext.get(mainClassVarName),
                         "Implementation-Version" to archiveVersion
                     )
                 )
             }
-            // All the dependencies needed by our application doesn't exists in the same classpath where our .jar resides.
-            // In order to make our application standalone, we must include all of its dependencies inside our application.
-            // Include all of its dependencies inside our application
 
-            // To add all of the dependencies otherwise a "NoClassDefFoundError" error
             from(sourceSets.main.get().output)
             dependsOn(configurations.runtimeClasspath)
 
             // Configurations.compile is a reference to the all of the artifacts defined in the compile configuration.
             from({
-                // Wrap each of the JAR files with the zipTree() method. The result is a collection of ZIP file trees
-
-                // configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+                // Wrap each of the JAR files with the zipTree() method.
+                // The result is a collection of ZIP file trees
                 configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
             })
         }
