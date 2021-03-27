543c5cf05086f2e61345c893bbe8ea9dc68fa994
Stefano <stefano.braggion@me.com>
2021-03-27 16:20:49 +0100

    freelancer requests

--- consulting/src/main/kotlin/it/unibo/lss/fcla/consulting/presentation/freelancer/FreelancerRequests.kt
+++ consulting/src/main/kotlin/it/unibo/lss/fcla/consulting/presentation/freelancer/FreelancerRequests.kt
@@ -1,2 +1,33 @@
 package it.unibo.lss.fcla.consulting.presentation
 
+import it.unibo.lss.fcla.consulting.domain.consulting.Date
+import it.unibo.lss.fcla.consulting.domain.freelancer.FreelancerId
+import java.time.LocalTime
+
+/**
+ *
+ */
+class CreateFreelancerAvailabilityForDayRequest(
+    val freelancerId: FreelancerId,
+    val availabilityDate: Date,
+    val fromTime: LocalTime,
+    val toTime: LocalTime
+) : IRequest
+
+/**
+ *
+ */
+class DeleteFreelancerAvailabilityForDayRequest(
+    val freelancerId: FreelancerId,
+    val availabilityDate: Date
+) : IRequest
+
+/**
+ *
+ */
+class UpdateFreelancerAvailabilityForDayRequest(
+    val freelancerId: FreelancerId,
+    val availabilityDate: Date,
+    val fromTime: LocalTime,
+    val toTime: LocalTime
+) : IRequest
