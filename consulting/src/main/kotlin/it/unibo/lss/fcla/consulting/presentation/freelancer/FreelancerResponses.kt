2e3d9e294e6113a604650797bbfb8844b736afc5
Stefano <stefano.braggion@me.com>
2021-03-27 16:59:52 +0100

    added console ui skeleton class

---
+++ consulting/src/main/kotlin/it/unibo/lss/fcla/consulting/presentation/freelancer/FreelancerResponses.kt
@@ -0,0 +1,14 @@
+package it.unibo.lss.fcla.consulting.presentation.freelancer
+
+import it.unibo.lss.fcla.consulting.presentation.IResponse
+
+/**
+ * @author Stefano Braggion
+ */
+
+/**
+ * Represents a textual response message
+ */
+class MessageResponse(
+    val message: String
+) : IResponse
