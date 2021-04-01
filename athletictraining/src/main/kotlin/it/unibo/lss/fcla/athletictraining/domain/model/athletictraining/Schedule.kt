905dc355b4fbc5c0930e280f0526663c984a0022
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-04-01 18:05:02 +0200

    Adds new Schedule value object

---
+++ athletictraining/src/main/kotlin/it/unibo/lss/fcla/athletictraining/domain/model/athletictraining/Schedule.kt
@@ -0,0 +1,29 @@
+package it.unibo.lss.fcla.athletictraining.domain.model.athletictraining
+
+import it.unibo.lss.fcla.athletictraining.domain.exception.BeginningOfScheduleCannotBeAfterEnd
+import java.time.LocalDate
+import java.time.LocalTime
+
+/**
+ * A ValueObject representing the schedule of a single
+ * [it.unibo.lss.fcla.athletictraining.domain.model.workout.Workout].
+ *
+ * @author Nicola Lasagni on 01/04/2021.
+ */
+data class Schedule(val day: LocalDate,
+                    val startTime: LocalTime,
+                    val endTime: LocalTime) {
+
+    init {
+        if (endTime.isBefore(startTime)) {
+            throw BeginningOfScheduleCannotBeAfterEnd()
+        }
+    }
+
+    fun overlaps(schedule: Schedule): Boolean {
+        return day.isEqual(schedule.day) &&
+                startTime.isBefore(schedule.endTime) &&
+                endTime.isAfter(schedule.startTime)
+    }
+
+}
