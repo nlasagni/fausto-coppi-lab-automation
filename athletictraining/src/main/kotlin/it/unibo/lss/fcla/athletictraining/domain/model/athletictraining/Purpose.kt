3a67c28040fc7aab1f9694b54d60fa18976a04c4
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-04-01 21:41:41 +0200

    Adds AthleticTraining as new Purpose, moves Purpose class inside athletictraining package and refactors tests

--- athletictraining/src/main/kotlin/it/unibo/lss/fcla/athletictraining/domain/model/athletictraining/Purpose.kt
+++ athletictraining/src/main/kotlin/it/unibo/lss/fcla/athletictraining/domain/model/athletictraining/Purpose.kt
@@ -1,11 +1,12 @@
-package it.unibo.lss.fcla.athletictraining.domain.model
+package it.unibo.lss.fcla.athletictraining.domain.model.athletictraining
 
 /**
- * The purpose that an athletic trainer wants the member to reach through a [TrainingPlan].
+ * The purpose that an athletic trainer wants a member to reach through an [AthleticTraining].
  *
  * @author Nicola Lasagni on 22/02/2021.
  */
 sealed class Purpose {
+    data class AthleticTraining(private val name: String = "Athletic Training") : Purpose()
     data class Strengthening(private val name: String = "Strengthening") : Purpose()
     data class InjuryRecovery(private val name: String = "Injury Recovery") : Purpose()
     data class Discharge(private val name: String = "Discharge") : Purpose()
