3e7465409957593cca91faa273d61846580b7b36
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-04-02 16:06:50 +0200

    Adds domain service related to exercise configurations appliance

---
+++ athletictraining/src/main/kotlin/it/unibo/lss/fcla/athletictraining/domain/service/ExerciseConfigurationApplier.kt
@@ -0,0 +1,12 @@
+package it.unibo.lss.fcla.athletictraining.domain.service
+
+import it.unibo.lss.fcla.athletictraining.domain.model.exercise.Configuration
+
+/**
+ * @author Nicola Lasagni on 02/04/2021.
+ */
+interface ExerciseConfigurationApplier {
+
+    fun applyConfiguration(configuration: Configuration)
+
+}
