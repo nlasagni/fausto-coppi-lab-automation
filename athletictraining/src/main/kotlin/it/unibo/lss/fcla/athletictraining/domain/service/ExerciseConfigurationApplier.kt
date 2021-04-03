a76179720f2a23670b074a4a7318b04057cb9eb3
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-04-03 14:55:26 +0200

    Adds domain service that applies exercise configuration to gym machine

--- athletictraining/src/main/kotlin/it/unibo/lss/fcla/athletictraining/domain/service/ExerciseConfigurationApplier.kt
+++ athletictraining/src/main/kotlin/it/unibo/lss/fcla/athletictraining/domain/service/ExerciseConfigurationApplier.kt
@@ -1,11 +1,22 @@
 package it.unibo.lss.fcla.athletictraining.domain.service
 
 import it.unibo.lss.fcla.athletictraining.domain.model.exercise.Configuration
+import it.unibo.lss.fcla.athletictraining.domain.model.exercise.Exercise
+import it.unibo.lss.fcla.athletictraining.domain.model.gymmachine.GymMachineId
 
 /**
+ * Domain service that applies a given [Configuration] of an [Exercise] to
+ * a gym machine.
+ *
  * @author Nicola Lasagni on 02/04/2021.
  */
 interface ExerciseConfigurationApplier {
 
-    fun applyConfiguration(configuration: Configuration)
+    /**
+     * Applies the desired [configuration] to the gym machine specified
+     * by the [GymMachineId] property of the [configuration].
+     * @return true if the operation completed successfully, false otherwise.
+     */
+    fun applyConfiguration(configuration: Configuration): Boolean
+
 }
