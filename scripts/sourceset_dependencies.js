e2aba91a390bef0eed7c3589fe52e5cd57bc88fa
Giordo94 <48984732+Giordo94@users.noreply.github.com>
2021-04-06 14:19:51 +0000

    Deploying to gh-pages from @ nlasagni/fausto-coppi-lab-automation@9410ba1a305949027b0d6d757d1954559853418b 🚀

--- scripts/sourceset_dependencies.js
+++ scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":athleticpreparation:dokkaHtml/main": [], ":consulting:dokkaHtml/main": [], ":reservation:dokkaHtml/main": []}'
+sourceset_dependencies = '{":athletictraining:dokkaHtml/main": [], ":consulting:dokkaHtml/main": [], ":reservation:dokkaHtml/main": []}'
