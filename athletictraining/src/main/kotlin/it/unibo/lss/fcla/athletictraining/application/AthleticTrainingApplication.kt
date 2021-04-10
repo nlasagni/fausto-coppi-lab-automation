3cd57affc415f039eb86a675e989050494c32fcf
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-04-10 18:22:45 +0200

    Adds empty application main and config for later use

---
+++ athletictraining/src/main/kotlin/it/unibo/lss/fcla/athletictraining/application/AthleticTrainingApplication.kt
@@ -0,0 +1,16 @@
+package it.unibo.lss.fcla.athletictraining.application
+
+/**
+ * @author Nicola Lasagni on 05/04/2021.
+ */
+class AthleticTrainingApplication {
+
+    fun run(args: Array<String>) {
+        print(args)
+        TODO("Create application")
+    }
+}
+
+fun main(args: Array<String>) {
+    AthleticTrainingApplication().run(args)
+}
---
+++ athletictraining/src/main/kotlin/it/unibo/lss/fcla/athletictraining/config/ApplicationConfig.kt
@@ -0,0 +1,6 @@
+package it.unibo.lss.fcla.athletictraining.config
+
+/**
+ * @author Nicola Lasagni on 05/04/2021.
+ */
+class ApplicationConfig
