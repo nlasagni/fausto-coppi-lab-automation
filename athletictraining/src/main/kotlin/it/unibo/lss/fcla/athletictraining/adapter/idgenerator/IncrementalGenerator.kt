e89dd9a35185fac23d90895b002b1d2459f20cba
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-04-11 19:17:45 +0200

    Adds doc and fices IncrementalGenerator

--- athletictraining/src/main/kotlin/it/unibo/lss/fcla/athletictraining/adapter/idgenerator/IncrementalGenerator.kt
+++ athletictraining/src/main/kotlin/it/unibo/lss/fcla/athletictraining/adapter/idgenerator/IncrementalGenerator.kt
@@ -3,14 +3,15 @@ package it.unibo.lss.fcla.athletictraining.adapter.idgenerator
 import it.unibo.lss.fcla.athletictraining.usecase.shared.output.IdGenerator
 
 /**
+ * An [IdGenerator] that generates id incrementally.
+ *
  * @author Nicola Lasagni on 11/04/2021.
  */
 class IncrementalGenerator : IdGenerator {
 
-    private var id = 0L
+    private var id = 1L
 
     override fun generate(): String {
-        id++
-        return id.toString()
+        return id++.toString()
     }
 }
