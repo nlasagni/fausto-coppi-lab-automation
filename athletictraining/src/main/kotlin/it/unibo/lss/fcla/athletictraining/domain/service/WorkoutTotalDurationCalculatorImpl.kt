6fe89c290dec4e730e35519f6b02e6ca2c36e1a1
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-04-11 23:56:49 +0200

    Fixes workout calculator service

--- athletictraining/src/main/kotlin/it/unibo/lss/fcla/athletictraining/domain/service/WorkoutTotalDurationCalculatorImpl.kt
+++ athletictraining/src/main/kotlin/it/unibo/lss/fcla/athletictraining/domain/service/WorkoutTotalDurationCalculatorImpl.kt
@@ -2,8 +2,11 @@ package it.unibo.lss.fcla.athletictraining.domain.service
 
 import it.unibo.lss.fcla.athletictraining.domain.model.exercise.Duration
 import it.unibo.lss.fcla.athletictraining.domain.model.exercise.Exercise
+import it.unibo.lss.fcla.athletictraining.domain.service.exception.WorkoutDurationMustBeGreaterThanZero
 
 /**
+ * Concrete implementation of an [WorkoutTotalDurationCalculator] service.
+ *
  * @author Nicola Lasagni on 11/04/2021.
  */
 class WorkoutTotalDurationCalculatorImpl : WorkoutTotalDurationCalculator {
@@ -14,6 +17,9 @@ class WorkoutTotalDurationCalculatorImpl : WorkoutTotalDurationCalculator {
             val snapshot = exercise.snapshot()
             totalSeconds +=
                 snapshot.durationOfExecution.seconds + snapshot.durationOfRest.seconds
+        }
+        if (totalSeconds == 0L) {
+            throw WorkoutDurationMustBeGreaterThanZero()
         }
         return Duration(totalSeconds)
     }
