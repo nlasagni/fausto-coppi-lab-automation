171d7dc964f65132ae79a976788b5384a2417b7f
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-04-12 09:58:06 +0200

    Adds all in-memory repository tests

--- athletictraining/src/test/kotlin/it/unibo/lss/fcla/athletictraining/adapter/repository/InMemoryActiveAthleticTrainingRepositoryTest.kt
+++ athletictraining/src/test/kotlin/it/unibo/lss/fcla/athletictraining/adapter/repository/InMemoryActiveAthleticTrainingRepositoryTest.kt
@@ -1,6 +1,8 @@
 package it.unibo.lss.fcla.athletictraining.adapter.repository
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.shouldBe
 import it.unibo.lss.fcla.athletictraining.adapter.idgenerator.UuidGenerator
 import it.unibo.lss.fcla.athletictraining.domain.model.activeathletictraining.ActiveAthleticTraining
 import it.unibo.lss.fcla.athletictraining.domain.model.activeathletictraining.ActiveAthleticTrainingId
@@ -13,9 +15,11 @@ import org.junit.jupiter.api.Assertions
 import java.time.LocalDate
 
 /**
+ * Test of the [InMemoryActiveAthleticTrainingRepository] class.
+ *
  * @author Nicola Lasagni on 07/03/2021.
  */
-class InMemoryAthleticPreparationRepositoryTest : FreeSpec({
+class InMemoryActiveAthleticTrainingRepositoryTest : FreeSpec({
 
     lateinit var repository: ActiveAthleticTrainingRepository
     lateinit var activeAthleticTrainingId: ActiveAthleticTrainingId
@@ -45,20 +49,32 @@ class InMemoryAthleticPreparationRepositoryTest : FreeSpec({
         )
     }
 
-    "An InMemoryAthleticPreparationRepository should" - {
-        "be able to persist AthleticPreparation" - {
-            val persistedAthleticPreparation = repository.add(activeAthleticTraining.snapshot())
+    "An InMemoryActiveAthleticTrainingRepository should" - {
+        "be able to persist active athletic trainings" - {
+            val persistedTraining = repository.add(activeAthleticTraining.snapshot())
             val snapshot = activeAthleticTraining.snapshot()
-            val persistedSnapshot = persistedAthleticPreparation
-            Assertions.assertEquals(snapshot, persistedSnapshot)
+            Assertions.assertEquals(snapshot, persistedTraining)
         }
-        "be able to find an AthleticPreparation by id" - {
+        "be able to update an already present active athletic training" - {
+            val persistedTraining = repository.add(activeAthleticTraining.snapshot())
+            val rehydratedAthleticTraining = ActiveAthleticTraining.rehydrate(persistedTraining)
+            val newPeriodEnd = period.endDay().plusDays(5)
+            rehydratedAthleticTraining.postponeTrainingPeriodEnd(newPeriodEnd)
+            val updatedTraining = repository.update(rehydratedAthleticTraining.snapshot())
+            updatedTraining.period.endDay().shouldBe(newPeriodEnd)
+        }
+        "be able to remove active athletic trainings from memory" - {
+            val persistedTraining = repository.add(activeAthleticTraining.snapshot())
+            repository.remove(persistedTraining)
+            repository.findAllByMemberId(memberId).shouldBeEmpty()
+        }
+        "be able to find an active athletic training by id" - {
             assert(repository.findById(ActiveAthleticTrainingId("0")) == null)
             val persistedAthleticPreparation = repository.add(activeAthleticTraining.snapshot())
             val persistedId = persistedAthleticPreparation.id
             assert(repository.findById(persistedId) != null)
         }
-        "be able to find an AthleticPreparation by memberId" - {
+        "be able to find an active athletic training by memberId" - {
             repository.add(activeAthleticTraining.snapshot())
             assert(repository.findAllByMemberId(memberId).isNotEmpty())
         }
