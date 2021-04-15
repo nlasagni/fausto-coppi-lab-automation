59a7a06fd1d7e649179d9203fa72dbbfcbe4b2e0
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-04-15 18:16:32 +0200

    Merges common jobs into same workflows

--- athletictraining/build.gradle.kts
+++ athletictraining/build.gradle.kts
@@ -1,3 +1,33 @@
 val myMainClass = "it.unibo.lss.fcla.athletictraining.AthleticTrainingApplicationStarterKt"
 
 ext.set("mainclass", myMainClass)
+
+val infrastructurePackage = "infrastructure"
+val adapterPackage = "adapter"
+val configPackage = ""
+val starterApplicationClass = ""
+
+tasks.jacocoTestReport {
+    classDirectories.setFrom(
+        sourceSets.getByName("main").output.asFileTree.matching {
+
+            exclude(
+                "**/adapter/**",
+                "**/config/**",
+                "**/infrastructure/**",
+                ""
+            )
+        }
+//        files(classDirectories.files.map {
+//            fileTree(it) {
+//                logger.log(LogLevel.WARN, "Directory is: ${it.absolutePath}")
+//                exclude(
+//                    "**/adapter/**",
+//                    "**/config/**",
+//                    "**/infrastructure/**",
+//                    ""
+//                )
+//            }
+//        })
+    )
+}
